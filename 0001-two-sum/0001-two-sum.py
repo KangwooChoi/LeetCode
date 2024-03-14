c562744d6d2d300d11392f47e19423544c617333
KangwooChoi <mickeychoi83@gmail.com>
2024-03-14 20:48:39 +0900

    Time: 58 ms (71.43%), Space: 17.8 MB (25.34%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Brute Force: Time O(n^2), Space O(1)
+        #for i in range(len(nums)):
+        #    for j in range(i+1, len(nums)):
+        #        if target == nums[i] + nums[j]:
+        #            return [i, j]
+
+        # Two-pass Hash Table: Time O(n), Space O(n)
+        #hashmap = {}
+        #for i in range(len(nums)):
+        #    hashmap[nums[i]] = i
+        #for i in range(len(nums)):
+        #    complement = target - nums[i]
+        #    if complement in hashmap and i != hashmap[complement]:
+        #        return [i, hashmap[complement]]
+                
+        # One-pass Hash Table: Time O(n), Space O(n)
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
