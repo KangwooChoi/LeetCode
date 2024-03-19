4a49671c4d14e78fde14a03102a2c49bdd3cb5e2
KangwooChoi <mickeychoi83@gmail.com>
2024-03-19 10:16:36 +0900

    Time: 161 ms (29.29%), Space: 18.9 MB (32.76%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,4 +1,12 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        nums = sorted([num**2 for num in nums])
-        return nums 
+        from heapq import heappop, heappush
+        square_list = []
+        for num in nums:
+            heappush(square_list, num**2)
+        ans = []
+        while square_list:
+            ans.append(heappop(square_list))
+        return ans
+        #nums = sorted([num**2 for num in nums])
+        #return nums 
