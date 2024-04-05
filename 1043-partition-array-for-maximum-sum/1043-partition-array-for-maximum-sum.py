97d3cf1489c5158606f70419a7bc16a94b2c80da
KangwooChoi <mickeychoi83@gmail.com>
2024-04-05 09:47:18 +0900

    Time: 609 ms (22.05%), Space: 16.7 MB (68%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -6,12 +6,12 @@ class Solution:
         for i in range(len(arr)):
             if i < k:
                 ans[i] = max(arr[:i+1])*(i+1)
-                print(f"ans[{i}] = {ans[i]}")
+                #print(f"ans[{i}] = {ans[i]}")
                 continue
             temp = []
             for j in range(1, k+1):
                 temp.append(ans[i-j]+j*max(arr[i-j+1:i+1]))
-            print(f"temp = {temp}")
+            #print(f"temp = {temp}")
             ans[i] = max(temp)
-            print(f"ans[{i}] = {ans[i]}")
+            #print(f"ans[{i}] = {ans[i]}")
         return ans[-1]
