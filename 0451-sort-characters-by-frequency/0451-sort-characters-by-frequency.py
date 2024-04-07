12284b4b8203785a37bb8939c99cebb037b99ce9
KangwooChoi <mickeychoi83@gmail.com>
2024-04-08 08:09:49 +0900

    Time: 67 ms (12.82%), Space: 19.6 MB (15.95%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,20 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        from heapq import heappop, heappush
+        s_dict = defaultdict()
+        for c in s:
+            if c not in s_dict.keys():
+                s_dict[c] = 1
+            else:
+                s_dict[c] += 1
+        queue = []
+        for key, val in s_dict.items():
+            heappush(queue, [-val, key])
+
+        ans = []
+        while queue:
+            val, key = heappop(queue)
+            for _ in range(-val):
+                ans.append(key)
+        return ans
+
