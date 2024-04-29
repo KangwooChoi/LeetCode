cb14b7c187f678ec13fd94337a54164d5140bec7
KangwooChoi <mickeychoi83@gmail.com>
2024-04-29 10:11:34 +0900

    Time: 1668 ms (5.1%), Space: 31 MB (50.32%) - LeetHub

---
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        def toBin(num):
+            cnt = 0
+            while num >= 1:
+                base[cnt] += num % 2
+                num //= 2
+                cnt += 1
+        
+        goal = defaultdict()
+        temp = k
+        cnt = 0
+        while temp >= 1:
+            goal[cnt] = temp % 2
+            temp //= 2
+            cnt += 1
+        #print("goal: ", goal)
+            
+        base = defaultdict(int)
+        for n in nums:
+            toBin(n)
+        #print("base: ", base)
+
+        if len(base) > len(goal):
+            for i in range(len(goal), len(base)):
+                goal[i] = 0
+        elif len(goal) > len(base):
+            for i in range(len(base), len(goal)):
+                base[i] = 0
+
+        ans = 0
+        for i in range(max(len(base), len(goal))):
+            if goal[i] == 0 and base[i] % 2 != 0:
+                ans += 1
+            elif goal[i] == 1 and  base[i] % 2 == 0:
+                ans += 1
+        return ans
