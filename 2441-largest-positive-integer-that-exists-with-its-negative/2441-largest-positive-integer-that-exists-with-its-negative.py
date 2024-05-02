8e556804e855eb42c0844c5921bdb2bfb67d0213
KangwooChoi <mickeychoi83@gmail.com>
2024-05-02 17:49:21 +0900

    Time: 111 ms (50.51%), Space: 16.7 MB (98.7%) - LeetHub

---
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        from heapq import heappop, heappush
+        pos = []
+        neg = []
+        for num in nums:
+            if num > 0:
+                heappush(pos, -1*num)
+            else:
+                heappush(neg, num)
+        if pos:
+            p = heappop(pos)
+        else:
+            return -1
+        if neg:
+            q = heappop(neg)
+        else:
+            return -1
+        #print("pos: ", pos, p)
+        #print("neg: ", neg, q)
+        while p and q:
+            #print("p, q: ", p, q)
+            if p == q:
+                return -1*p
+            elif p < q:
+                if pos:
+                    p = heappop(pos)
+                else:
+                    return -1
+            elif p > q:
+                if neg:
+                    q = heappop(neg)
+                else:
+                    return -1
