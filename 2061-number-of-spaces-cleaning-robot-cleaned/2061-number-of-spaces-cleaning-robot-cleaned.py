35a261f82a5194eb9d79bef9d7e4e62ecd7290ca
KangwooChoi <mickeychoi83@gmail.com>
2024-05-16 10:03:36 +0900

    Time: 324 ms (42.68%), Space: 19.4 MB (48.78%) - LeetHub

---
+++ 2061-number-of-spaces-cleaning-robot-cleaned/2061-number-of-spaces-cleaning-robot-cleaned.py
@@ -0,0 +1,34 @@
+class Solution:
+    def numberOfCleanRooms(self, room: List[List[int]]) -> int:
+        ans = 1
+        room[0][0] = 2
+        dx = [0, 1, 0, -1] 
+        dy = [1, 0, -1, 0]
+        x, y, i = 0, 0, 0
+        m, n = len(room), len(room[0])
+        cnt = 0
+        while True:
+            nx, ny = x+dx[i], y+dy[i]
+            if 0 <= nx < m and 0 <= ny < n:
+                if room[nx][ny] == 1:
+                    cnt += 1
+                    i += 1
+                    i %= 4
+                    if cnt == 4:
+                        return ans
+                elif room[nx][ny] == 2:
+                    if cnt > m*n:
+                        return ans
+                    x, y = nx, ny
+                    cnt += 1
+                elif room[nx][ny] == 0:
+                    cnt = 0
+                    room[nx][ny] = 2
+                    x, y = nx, ny
+                    ans += 1
+            else:
+                cnt += 1
+                if cnt == 4:
+                    return ans
+                i += 1
+                i %= 4
