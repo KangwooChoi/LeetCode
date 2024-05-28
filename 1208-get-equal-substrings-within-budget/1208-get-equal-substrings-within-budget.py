2256e828391dc49093e805589fcf21529690dfe0
KangwooChoi <mickeychoi83@gmail.com>
2024-05-28 09:25:53 +0900

    Time: 73 ms (17.44%), Space: 18.3 MB (10.58%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -10,11 +10,12 @@ class Solution:
         conti = 0
         for i in range(len(s)):
             window += diff[i]
+            conti += 1
             if window <= maxCost:
-                conti += 1
                 ans = max(ans, conti)
             else:
-                window -= diff[prev]
-                prev += 1
-                conti -= 1
+                while window > maxCost:
+                    window -= diff[prev]
+                    prev += 1
+                    conti -= 1
         return ans
