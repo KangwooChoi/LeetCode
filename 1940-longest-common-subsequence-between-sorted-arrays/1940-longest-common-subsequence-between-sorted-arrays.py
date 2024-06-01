ab4303651253cf174c918a757b5a8430e44f05b7
KangwooChoi <mickeychoi83@gmail.com>
2024-06-01 11:46:22 +0900

    Time: 240 ms (5.13%), Space: 17.1 MB (16.03%) - LeetHub

---
+++ 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestCommonSubsequence(self, arrays: List[List[int]]) -> List[int]:
+        ans = []
+        locs = [0] * len(arrays) 
+        #comp = max([arrays[i][0] for i in range(len(arrays))])
+        while True:
+            comp = 0
+            for i in range(len(arrays)):
+                if locs[i] == len(arrays[i]):
+                    return ans
+                comp = max(comp, arrays[i][locs[i]])
+            print("comp: ", comp)
+            cnt = 0
+            for i in range(len(arrays)):
+                if locs[i] == len(arrays[i]):
+                    return ans
+                while arrays[i][locs[i]] <= comp:
+                    if arrays[i][locs[i]] == comp:
+                        cnt += 1
+                        locs[i] += 1
+                        break
+                    locs[i] += 1
+                    if locs[i] == len(arrays[i]):
+                        return ans
+            print("cnt: ", cnt)
+            print("loc: ", locs)
+            if cnt == len(arrays):
+                ans.append(comp)
+            print("ans: ", ans)
