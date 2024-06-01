cbf552376bb6198867b370546b90270446ae4833
KangwooChoi <mickeychoi83@gmail.com>
2024-06-01 11:49:10 +0900

    Time: 195 ms (90.38%), Space: 16.9 MB (76.92%) - LeetHub

--- 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.py
+++ 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.py
@@ -1,29 +1,33 @@
 class Solution:
     def longestCommonSubsequence(self, arrays: List[List[int]]) -> List[int]:
-        ans = []
-        locs = [0] * len(arrays) 
-        #comp = max([arrays[i][0] for i in range(len(arrays))])
-        while True:
-            comp = 0
-            for i in range(len(arrays)):
-                if locs[i] == len(arrays[i]):
-                    return ans
-                comp = max(comp, arrays[i][locs[i]])
-            #print("comp: ", comp)
-            cnt = 0
-            for i in range(len(arrays)):
-                if locs[i] == len(arrays[i]):
-                    return ans
-                while arrays[i][locs[i]] <= comp:
-                    if arrays[i][locs[i]] == comp:
-                        cnt += 1
-                        locs[i] += 1
-                        break
-                    locs[i] += 1
-                    if locs[i] == len(arrays[i]):
-                        return ans
-            #print("cnt: ", cnt)
-            #print("loc: ", locs)
-            if cnt == len(arrays):
-                ans.append(comp)
-            #print("ans: ", ans)
+        ans = set(arrays[0])
+        for i in range(1, len(arrays)):
+            ans = ans.intersection(set(arrays[i]))
+        return sorted(ans)
+        #ans = []
+        #locs = [0] * len(arrays) 
+        ##comp = max([arrays[i][0] for i in range(len(arrays))])
+        #while True:
+        #    comp = 0
+        #    for i in range(len(arrays)):
+        #        if locs[i] == len(arrays[i]):
+        #            return ans
+        #        comp = max(comp, arrays[i][locs[i]])
+        #    #print("comp: ", comp)
+        #    cnt = 0
+        #    for i in range(len(arrays)):
+        #        if locs[i] == len(arrays[i]):
+        #            return ans
+        #        while arrays[i][locs[i]] <= comp:
+        #            if arrays[i][locs[i]] == comp:
+        #                cnt += 1
+        #                locs[i] += 1
+        #                break
+        #            locs[i] += 1
+        #            if locs[i] == len(arrays[i]):
+        #                return ans
+        #    #print("cnt: ", cnt)
+        #    #print("loc: ", locs)
+        #    if cnt == len(arrays):
+        #        ans.append(comp)
+        #    #print("ans: ", ans)
