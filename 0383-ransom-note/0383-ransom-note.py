02a5b045c1007068e2871a7780920075eeacc743
KangwooChoi <mickeychoi83@gmail.com>
2024-06-29 19:52:41 +0900

    Time: 61 ms (44.46%), Space: 16.7 MB (64.48%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,21 +1,28 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        rDict = defaultdict(str)
-        mDict = defaultdict(str)
-        for c in ransomNote:
-            if rDict[c]:
-                rDict[c] += 1
-            else:
-                rDict[c] = 1
-
-        for c in magazine:
-            if mDict[c]:
-                mDict[c] += 1
-            else:
-                mDict[c] = 1
-        
-        for key, cnt in rDict.items():
-            if not mDict[key] or mDict[key] < cnt:
+        from collections import Counter
+        rCnter = Counter(ransomNote)
+        mCnter = Counter(magazine)
+        for key, cnt in rCnter.items():
+            if key not in mCnter or mCnter[key] < cnt:
                 return False
         return True
+        #rDict = defaultdict(str)
+        #mDict = defaultdict(str)
+        #for c in ransomNote:
+        #    if rDict[c]:
+        #        rDict[c] += 1
+        #    else:
+        #        rDict[c] = 1
+#
+        #for c in magazine:
+        #    if mDict[c]:
+        #        mDict[c] += 1
+        #    else:
+        #        mDict[c] = 1
+        #
+        #for key, cnt in rDict.items():
+        #    if not mDict[key] or mDict[key] < cnt:
+        #        return False
+        #return True
                    
