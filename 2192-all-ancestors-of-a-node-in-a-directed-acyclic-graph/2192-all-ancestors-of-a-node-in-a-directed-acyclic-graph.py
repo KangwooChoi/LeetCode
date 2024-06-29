448d6e66a1ea3694dad11ccb8947d361bde89feb
KangwooChoi <mickeychoi83@gmail.com>
2024-06-29 12:09:02 +0900

    Time: 897 ms (11.3%), Space: 51.4 MB (18.22%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,21 +1,48 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        direct_p = [[] for _ in range(n)] 
-        ans = [[] for _ in range(n)] 
+        direct_p = [[] for _ in range(n)]
         for s, e in edges:
             direct_p[e].append(s)
-        for i in range(len(direct_p)):
-            q = copy.deepcopy(direct_p[i])
-            while q:
-                now = q.pop()
-                ans[i].append(now)
-                for new in direct_p[now]:
-                    if new not in q:
-                        q.append(new)
-        for i in range(len(ans)): 
-            ans[i] = list(set(ans[i]))
-            ans[i].sort()
+        
+        ans = []
+
+        for i in range(n):
+            ancestors = []
+            visited = set()
+            self.find_children(i, direct_p, visited)
+            for node in range(n):
+                if node == i:
+                    continue
+                if node in visited:
+                    ancestors.append(node)
+            ans.append(ancestors)
+
         return ans
+    
+    def find_children(self, curr, p, v):
+        v.add(curr)
+        for neighbour in p[curr]:
+            if neighbour not in v:
+                self.find_children(neighbour, p, v)
+
+
+
+        #direct_p = [[] for _ in range(n)] 
+        #ans = [[] for _ in range(n)] 
+        #for s, e in edges:
+        #    direct_p[e].append(s)
+        #for i in range(len(direct_p)):
+        #    q = copy.deepcopy(direct_p[i])
+        #    while q:
+        #        now = q.pop()
+        #        ans[i].append(now)
+        #        for new in direct_p[now]:
+        #            if new not in q:
+        #                q.append(new)
+        #for i in range(len(ans)): 
+        #    ans[i] = list(set(ans[i]))
+        #    ans[i].sort()
+        #return ans
             
 
         
