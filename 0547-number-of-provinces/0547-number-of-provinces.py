ceb0e099cbe45f58c5b425d2d9984377a6eaf7c5
KangwooChoi <mickeychoi83@gmail.com>
2024-06-29 14:26:22 +0900

    Time: 195 ms (18.66%), Space: 17.2 MB (90.59%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,22 +1,49 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def union(i, j):
-            p_i = parents[i]
-            p_j = parents[j]
-            if p_i != p_j:
-                for k in range(0, n):
-                    if parents[k] == p_j:
-                        parents[k] = p_i
+        # building our root list
+        l1 = [0]*len(isConnected[0])
+        for i in range(len(l1)):
+            l1[i] = i
+        
+        # returns root of existing vertex
+        def get_root(x):
+            return l1[x]
+        
+        # update root of existing index
+        def union(x,y):
+            Rx = get_root(x)
+            Ry = get_root(y)
 
-        def find(k):
-            pass
-            
-        n = len(isConnected)
-        parents = [i for i in range(n)]
-        for i in range(n):
-            for j in range(i+1, n):
-                if isConnected[i][j]:
+            if Rx != Ry:
+                for i in range(0,len(l1)):
+                    if l1[i] == Ry:
+                        l1[i] = Rx
+
+        # traversing only the upper traingular matrix to save time
+        for i in range(0,len(isConnected)):
+            for j in range(i+1,len(isConnected[0])):
+                if isConnected[i][j] == 1:
                     union(i,j)
-
-        #print(parents)
-        return len(set(parents))
+            
+        return len(set(l1))
+    #def findCircleNum(self, isConnected: List[List[int]]) -> int:
+    #    def union(i, j):
+    #        p_i = parents[i]
+    #        p_j = parents[j]
+    #        if p_i != p_j:
+    #            for k in range(0, n):
+    #                if parents[k] == p_j:
+    #                    parents[k] = p_i
+#
+    #    def find(k):
+    #        pass
+    #        
+    #    n = len(isConnected)
+    #    parents = [i for i in range(n)]
+    #    for i in range(n):
+    #        for j in range(i+1, n):
+    #            if isConnected[i][j]:
+    #                union(i,j)
+#
+    #    #print(parents)
+    #    return len(set(parents))
