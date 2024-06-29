edec807045f4c9ae3740dc538b3526c02f24020a
KangwooChoi <mickeychoi83@gmail.com>
2024-06-29 14:28:19 +0900

    Time: 183 ms (59.26%), Space: 17.6 MB (22.17%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,31 +1,53 @@
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        # building our root list
-        l1 = [0]*len(isConnected[0])
-        for i in range(len(l1)):
-            l1[i] = i
+    def findCircleNum(self, M: List[List[int]]) -> int:
+       
+        if not M:
+            return 0
         
-        # returns root of existing vertex
-        def get_root(x):
-            return l1[x]
+        n = len(M)
+        visit = [False]*n
         
-        # update root of existing index
-        def union(x,y):
-            Rx = get_root(x)
-            Ry = get_root(y)
-
-            if Rx != Ry:
-                for i in range(0,len(l1)):
-                    if l1[i] == Ry:
-                        l1[i] = Rx
-
-        # traversing only the upper traingular matrix to save time
-        for i in range(0,len(isConnected)):
-            for j in range(i+1,len(isConnected[0])):
-                if isConnected[i][j] == 1:
-                    union(i,j)
-            
-        return len(set(l1))
+        def dfs(u):
+            for v in range(n):
+                if M[u][v] ==1 and visit[v] == False:
+                    visit[v] = True
+                    dfs(v)
+        
+        count = 0
+        for i in range(n):
+            if visit[i] == False:
+                count += 1
+                visit[i] = True
+                dfs(i)
+        
+        return count
+    #def findCircleNum(self, isConnected: List[List[int]]) -> int:
+    #    # building our root list
+    #    l1 = [0]*len(isConnected[0])
+    #    for i in range(len(l1)):
+    #        l1[i] = i
+    #    
+    #    # returns root of existing vertex
+    #    def get_root(x):
+    #        return l1[x]
+    #    
+    #    # update root of existing index
+    #    def union(x,y):
+    #        Rx = get_root(x)
+    #        Ry = get_root(y)
+#
+    #        if Rx != Ry:
+    #            for i in range(0,len(l1)):
+    #                if l1[i] == Ry:
+    #                    l1[i] = Rx
+#
+    #    # traversing only the upper traingular matrix to save time
+    #    for i in range(0,len(isConnected)):
+    #        for j in range(i+1,len(isConnected[0])):
+    #            if isConnected[i][j] == 1:
+    #                union(i,j)
+    #        
+    #    return len(set(l1))
     #def findCircleNum(self, isConnected: List[List[int]]) -> int:
     #    def union(i, j):
     #        p_i = parents[i]
