52c6205f051f482ba65620b5436544a8d125b750
KangwooChoi <mickeychoi83@gmail.com>
2024-07-03 09:19:05 +0900

    Time: 285 ms (32.08%), Space: 27.5 MB (39.86%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -5,9 +5,8 @@ class Solution:
             return 0
         nums.sort()
         nums = deque(nums)
-        for i in range(3):
-            case0 = nums[-1] - nums[3]
-            case1 = nums[-2] - nums[2]
-            case2 = nums[-3] - nums[1]
-            case3 = nums[-4] - nums[0]
+        case0 = nums[-1] - nums[3]
+        case1 = nums[-2] - nums[2]
+        case2 = nums[-3] - nums[1]
+        case3 = nums[-4] - nums[0]
         return min(case0, case1, case2, case3)
