184f2c1558ce42ce83e3a6d11b7c1484a5c61729
KangwooChoi <mickeychoi83@gmail.com>
2024-07-22 10:40:29 +0900

    Time: 798 ms (55.07%), Space: 17.8 MB (82.61%) - LeetHub

--- 2093-minimum-cost-to-reach-city-with-discounts/2093-minimum-cost-to-reach-city-with-discounts.py
+++ 2093-minimum-cost-to-reach-city-with-discounts/2093-minimum-cost-to-reach-city-with-discounts.py
@@ -1,52 +1,82 @@
 class Solution:
-    def minimumCost(self, n: int, highways: List[List[int]], discounts: int) -> int:
-        graph = [[] for _ in range(n)]
-        for highway in highways:
-            u, v, toll = highway
-            graph[u].append((v, toll))
-            graph[v].append((u, toll))
+    def minimumCost(self, n: int, highways: List[List[int]], discounts: int) -> int: 
+        # Solutions by user
 
-        pq = [(0, 0, 0)]
+        pq = [(0, 0, discounts)]
+        graph = collections.defaultdict(list)
+        visited = dict()
+        cost = dict()
 
-        dist = [[float("inf")] * (discounts + 1) for _ in range(n)]
-        dist[0][0] = 0
-        print(dist)
-
-        visited = [[False] * (discounts + 1) for _ in range(n)]
-        print(visited)
-
+        for a, b, dist in highways:
+            graph[a].append(b)
+            graph[b].append(a)
+            cost[(a,b)] = dist
+            cost[(b,a)] = dist
+        
         while pq:
-            curr_cost, city, discount_used = heapq.heappop(pq)
-            if visited[city][discount_used]:
+            curr_cost, node, curr_disc = heapq.heappop(pq)
+            if node in visited and curr_disc <= visited[node]:
                 continue
-            visited[city][discount_used] = curr_cost + toll
-            
-            for neighbor, toll in graph[city]:
-                if curr_cost + toll < dist[neighbor][discount_used]:
-                    dist[neighbor][discount_used] = curr_cost + toll
-                    heapq.heappush(
-                        pq,
-                        (
-                            dist[neighbor][discount_used],
-                            neighbor,
-                            discount_used
-                        )
-                    )
+            visited[node] = curr_disc
+            if node == n - 1:
+                return curr_cost
+            for neigh in graph[node]:
+                if curr_disc > 0:
+                    heapq.heappush(pq, (cost[(node, neigh)] // 2 + curr_cost, neigh, curr_disc - 1))
+                heapq.heappush(pq, (cost[(node, neigh)] + curr_cost, neigh, curr_disc))
                 
-                if discount_used < discounts:
-                    new_cost_with_discount = curr_cost + toll // 2
-                    if new_cost_with_discount < dist[neighbor][discount_used + 1]:
-                        dist[neighbor][discount_used+1] = new_cost_with_discount
-                        heapq.heappush(
-                            pq,
-                            (
-                                new_cost_with_discount,
-                                neighbor,
-                                discount_used + 1,
-                            ),
-                        )
-        min_cost = min(dist[n-1])
-        return -1 if min_cost == float("inf") else min_cost
+        return -1
+
+
+        # Editorial
+
+        #graph = [[] for _ in range(n)]
+        #for highway in highways:
+        #    u, v, toll = highway
+        #    graph[u].append((v, toll))
+        #    graph[v].append((u, toll))
+#
+        #pq = [(0, 0, 0)]
+#
+        #dist = [[float("inf")] * (discounts + 1) for _ in range(n)]
+        #dist[0][0] = 0
+        #print(dist)
+#
+        #visited = [[False] * (discounts + 1) for _ in range(n)]
+        #print(visited)
+#
+        #while pq:
+        #    curr_cost, city, discount_used = heapq.heappop(pq)
+        #    if visited[city][discount_used]:
+        #        continue
+        #    visited[city][discount_used] = curr_cost + toll
+        #    
+        #    for neighbor, toll in graph[city]:
+        #        if curr_cost + toll < dist[neighbor][discount_used]:
+        #            dist[neighbor][discount_used] = curr_cost + toll
+        #            heapq.heappush(
+        #                pq,
+        #                (
+        #                    dist[neighbor][discount_used],
+        #                    neighbor,
+        #                    discount_used
+        #                )
+        #            )
+        #        
+        #        if discount_used < discounts:
+        #            new_cost_with_discount = curr_cost + toll // 2
+        #            if new_cost_with_discount < dist[neighbor][discount_used + 1]:
+        #                dist[neighbor][discount_used+1] = new_cost_with_discount
+        #                heapq.heappush(
+        #                    pq,
+        #                    (
+        #                        new_cost_with_discount,
+        #                        neighbor,
+        #                        discount_used + 1,
+        #                    ),
+        #                )
+        #min_cost = min(dist[n-1])
+        #return -1 if min_cost == float("inf") else min_cost
                         
 #class Solution:
 #    def minimumCost(self, n: int, highways: List[List[int]], discounts: int) -> int:
