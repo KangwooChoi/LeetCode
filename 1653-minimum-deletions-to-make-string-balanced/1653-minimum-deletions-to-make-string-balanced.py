bfc55416ac6ceda2db06125e3af88ae069fe9c30
KangwooChoi <mickeychoi83@gmail.com>
2024-07-30 10:06:13 +0900

    Time: 619 ms (21.67%), Space: 22.2 MB (13.67%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -3,20 +3,24 @@ class Solution:
         n = len(s)
         count_a = [0] * n
         count_b = [0] * n
-        b_count = 0 
-        for i in range(n):
-            count_b[i] = b_count
-            if s[i] == 'b':
-                b_count += 1
+        #b_count = 0 
+        #for i in range(n):
+        #    count_b[i] = b_count
+        #    if s[i] == 'b':
+        #        b_count += 1
 
         a_count = 0
         for i in range(n-1, -1, -1):
             count_a[i] = a_count
             if s[i] == 'a':
                 a_count += 1
-
+        
+        b_count = 0
         ans = n
         for i in range(n):
+            count_b[i] = b_count
+            if s[i] == 'b':
+                b_count += 1
             ans = min(ans, count_a[i] + count_b[i])
         
         return ans
