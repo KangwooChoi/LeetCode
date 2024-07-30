315e76d4d6e7386b4c811f045b172c0b19d79de9
KangwooChoi <mickeychoi83@gmail.com>
2024-07-30 10:07:46 +0900

    Time: 155 ms (97%), Space: 17.6 MB (92.33%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,29 +1,37 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        n = len(s)
-        count_a = [0] * n
-        count_b = [0] * n
-        #b_count = 0 
+        ans, count = 0, 0
+        for i in s:
+            if i == 'b':
+                count += 1
+            elif count:
+                ans += 1
+                count -= 1
+        return ans
+        #n = len(s)
+        #count_a = [0] * n
+        #count_b = [0] * n
+        ##b_count = 0 
+        ##for i in range(n):
+        ##    count_b[i] = b_count
+        ##    if s[i] == 'b':
+        ##        b_count += 1
+#
+        #a_count = 0
+        #for i in range(n-1, -1, -1):
+        #    count_a[i] = a_count
+        #    if s[i] == 'a':
+        #        a_count += 1
+        #
+        #b_count = 0
+        #ans = n
         #for i in range(n):
         #    count_b[i] = b_count
         #    if s[i] == 'b':
         #        b_count += 1
-
-        a_count = 0
-        for i in range(n-1, -1, -1):
-            count_a[i] = a_count
-            if s[i] == 'a':
-                a_count += 1
-        
-        b_count = 0
-        ans = n
-        for i in range(n):
-            count_b[i] = b_count
-            if s[i] == 'b':
-                b_count += 1
-            ans = min(ans, count_a[i] + count_b[i])
-        
-        return ans
+        #    ans = min(ans, count_a[i] + count_b[i])
+        #
+        #return ans
         #a_cnt, b_cnt = 0, 0
         #ans = 0
         #left, right = 0, 0
