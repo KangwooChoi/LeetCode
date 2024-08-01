8c07a30f99c2798e1a7c179f62c0aa8e2f064453
KangwooChoi <mickeychoi83@gmail.com>
2024-08-01 10:57:34 +0900

    Time: 43 ms (69.07%), Space: 17.1 MB (51%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.py
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.py
@@ -1,23 +1,44 @@
 class Solution:
     def longestRepeatingSubstring(self, s: str) -> int:
-        length = len(s)
-        suffixes = []
+        start, end = 0, len(s)
+        while start <= end:
+            mid = (start + end) // 2
+            if self._has_repeating(s, mid):
+                start = mid + 1
+            else:
+                end = mid - 1
+        return start - 1
 
-        for i in range(length):
-            suffixes.append(s[i:])
-        suffixes.sort()
+    
+    def _has_repeating(self, s:str, length: int) -> bool:
+        seen_subs = set()
+        for i in range(len(s) - length + 1):
+            sub = s[i:i+length]
+            if sub in seen_subs:
+                return True
+            seen_subs.add(sub)
+        return False
 
-        max_length = 0
-        for i in range(1, length):
-            cand = 0
-            while (
-                cand < min(len(suffixes[i]), len(suffixes[i-1]))
-                and suffixes[i][cand] == suffixes[i-1][cand]
-            ):
-                cand += 1
-            
-            max_length = max(max_length, cand)
-        return max_length
+        
+        #length = len(s)
+        #suffixes = []
+#
+        #for i in range(length):
+        #    suffixes.append(s[i:])
+        #suffixes.sort()
+        ##print(suffixes)
+#
+        #max_length = 0
+        #for i in range(1, length):
+        #    cand = 0
+        #    while (
+        #        cand < min(len(suffixes[i]), len(suffixes[i-1]))
+        #        and suffixes[i][cand] == suffixes[i-1][cand]
+        #    ):
+        #        cand += 1
+        #    
+        #    max_length = max(max_length, cand)
+        #return max_length
 
         #max_length = len(s) - 1
 #
