2ac8ecb7eb2a41abe397a07346feec93ed5bbc2a
KangwooChoi <mickeychoi83@gmail.com>
2024-08-01 10:36:23 +0900

    Time: 115 ms (43.65%), Space: 16.9 MB (70.6%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.py
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.py
@@ -1,17 +1,36 @@
 class Solution:
     def longestRepeatingSubstring(self, s: str) -> int:
-        max_length = len(s) - 1
+        length = len(s)
+        suffixes = []
 
-        while max_length > 0:
-            seen_subs = defaultdict(int)
-            for start in range(len(s) - max_length + 1):
-                end = start
-                curr_sub = s[end:end + max_length]
-                if curr_sub in seen_subs:
-                    return max_length
-                seen_subs[curr_sub] = 1
-            max_length -= 1
-        return 0
+        for i in range(length):
+            suffixes.append(s[i:])
+        suffixes.sort()
+
+        max_length = 0
+        for i in range(1, length):
+            cand = 0
+            while (
+                cand < min(len(suffixes[i]), len(suffixes[i-1]))
+                and suffixes[i][cand] == suffixes[i-1][cand]
+            ):
+                cand += 1
+            
+            max_length = max(max_length, cand)
+        return max_length
+
+        #max_length = len(s) - 1
+#
+        #while max_length > 0:
+        #    seen_subs = defaultdict(int)
+        #    for start in range(len(s) - max_length + 1):
+        #        end = start
+        #        curr_sub = s[end:end + max_length]
+        #        if curr_sub in seen_subs:
+        #            return max_length
+        #        seen_subs[curr_sub] = 1
+        #    max_length -= 1
+        #return 0
         #all_subs = defaultdict(int)  
         #n = len(s)
         #for i in range(n):
