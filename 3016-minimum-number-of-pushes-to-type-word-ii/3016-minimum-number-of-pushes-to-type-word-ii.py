34e4c90ab023ce1a393d090655f5e6a45796860e
KangwooChoi <mickeychoi83@gmail.com>
2024-08-06 09:43:46 +0900

    Time: 218 ms (31.74%), Space: 17.6 MB (73.03%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,10 +1,16 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        counter = [0] * 26
+        counter = defaultdict(int)
         for c in word:
-            index = ord(c) - ord('a')
-            counter[index] += 1
+            counter[c] += 1
+        counter = list(counter.values())
         counter.sort(reverse=True)
-        ans = 0
-        ans += sum(counter[0:8]) + 2 * sum(counter[8:16]) + 3 * sum(counter[16:24]) + 4 * sum(counter[24:])
-        return ans
+        return sum(counter[0:8]) + 2 * sum(counter[8:16]) + 3 * sum(counter[16:24]) + 4 * sum(counter[24:])
+        #counter = [0] * 26
+        #for c in word:
+        #    index = ord(c) - ord('a')
+        #    counter[index] += 1
+        #counter.sort(reverse=True)
+        #ans = 0
+        #ans += sum(counter[0:8]) + 2 * sum(counter[8:16]) + 3 * sum(counter[16:24]) + 4 * sum(counter[24:])
+        #return ans
