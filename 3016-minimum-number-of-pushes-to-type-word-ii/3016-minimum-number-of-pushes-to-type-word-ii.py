03486a97364368e6d1acb6552ff2e0c6a9080892
KangwooChoi <mickeychoi83@gmail.com>
2024-08-06 09:46:10 +0900

    Time: 154 ms (60.14%), Space: 17.6 MB (36.75%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        counter = defaultdict(int)
-        for c in word:
-            counter[c] += 1
-        counter = list(counter.values())
+        from collections import Counter
+        freq_map = Counter(word)
+        counter = list(freq_map.values())
         counter.sort(reverse=True)
         return sum(counter[0:8]) + 2 * sum(counter[8:16]) + 3 * sum(counter[16:24]) + 4 * sum(counter[24:])
+
         #counter = [0] * 26
         #for c in word:
         #    index = ord(c) - ord('a')
