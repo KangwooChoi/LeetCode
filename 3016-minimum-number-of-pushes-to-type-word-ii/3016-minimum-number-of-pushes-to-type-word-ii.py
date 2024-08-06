17296cc5a0b6ee1872f2ba5292735f2136c00657
KangwooChoi <mickeychoi83@gmail.com>
2024-08-06 09:47:40 +0900

    Time: 158 ms (52.98%), Space: 17.5 MB (90.45%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,10 +1,15 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        from collections import Counter
-        freq_map = Counter(word)
-        counter = list(freq_map.values())
-        counter.sort(reverse=True)
-        return sum(counter[0:8]) + 2 * sum(counter[8:16]) + 3 * sum(counter[16:24]) + 4 * sum(counter[24:])
+        cnt = Counter(word)
+        ans = 0
+        for i, x in enumerate(sorted(cnt.values(), reverse=True)):
+            ans += (i // 8 + 1) * x
+        return ans
+        #from collections import Counter
+        #freq_map = Counter(word)
+        #counter = list(freq_map.values())
+        #counter.sort(reverse=True)
+        #return sum(counter[0:8]) + 2 * sum(counter[8:16]) + 3 * sum(counter[16:24]) + 4 * sum(counter[24:])
 
         #counter = [0] * 26
         #for c in word:
