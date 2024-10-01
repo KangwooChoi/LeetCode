1fa404733a911d72814dc3cf65f6b35f0f65c3ad
KangwooChoi <mickeychoi83@gmail.com>
2024-10-01 14:07:45 +0900

    Time: 517 ms (37.1%), Space: 30.3 MB (91.94%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        residue = [0] * k
+        for num in arr:
+            q = num % k
+            comp = (k - q) % k
+            if residue[comp]:
+                residue[comp] -= 1
+            else:
+                residue[q] += 1
+        if any(residue):
+            return False
+        else:
+            return True
+        if sum(residue) == 0:
+            return True
+        else:
+            return False
