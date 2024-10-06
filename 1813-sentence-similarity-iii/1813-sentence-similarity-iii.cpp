f8168654b5761d040618fbda42974854286ef0d1
KangwooChoi <mickeychoi83@gmail.com>
2024-10-06 09:49:09 +0900

    Time: 5 ms (8.76%), Space: 8.7 MB (22.58%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool areSentencesSimilar(string sentence1, string sentence2) {
+        istringstream iss1(sentence1);
+        vector<string> vec1((istream_iterator<string>{iss1}),
+                             istream_iterator<string>());
+        istringstream iss2(sentence2);
+        vector<string> vec2((istream_iterator<string>{iss2}),
+                             istream_iterator<string>());
+
+        while (!vec1.empty() && !vec2.empty() && vec1.front() == vec2.front()) {
+            vec1.erase(vec1.begin());
+            vec2.erase(vec2.begin());
+        } 
+
+        while (!vec1.empty() && !vec2.empty() && vec1.back() == vec2.back()) {
+            vec1.pop_back();
+            vec2.pop_back();
+        }
+
+        return vec1.empty() || vec2.empty();
+    }
+};
