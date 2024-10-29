3a3e79c4fa5b9ed80b5cf2caa711599219591816
KangwooChoi <mickeychoi83@gmail.com>
2024-10-29 09:47:39 +0900

    Time: 64 ms (39.39%), Space: 16.9 MB (25.03%) - LeetHub

---
+++ 0265-paint-house-ii/0265-paint-house-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCostII(self, costs: List[List[int]]) -> int:
+
+        n = len(costs)
+        if n == 0: return 0
+        k = len(costs[0])
+
+        for house in range(1, n):
+            for color in range(k):
+                best = math.inf
+                for previous_color in range(k):
+                    if color == previous_color: continue
+                    best = min(best, costs[house - 1][previous_color])
+                costs[house][color] += best
+
+        return min(costs[-1])
