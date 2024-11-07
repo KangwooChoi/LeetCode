4151c98b9569e3b59c51d093edee8c3f951ab664
KangwooChoi <mickeychoi83@gmail.com>
2024-11-07 10:00:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestCombination(self, candidates: List[int]) -> int:
+        def counter(num, arr):
+            i = 0
+            while num:
+                arr[i] += num % 2
+                num //= 2
+                i += 1
+        bit_arr = [0] * 20
+        for cand in candidates:
+            counter(cand, bit_arr)
+            #print(bit_arr) 
+        return max(bit_arr)
