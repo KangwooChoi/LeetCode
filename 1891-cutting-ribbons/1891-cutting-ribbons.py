899aa961d15aea8ff8ae7ffa8017115647a2883b
KangwooChoi <mickeychoi83@gmail.com>
2024-12-08 11:40:16 +0900

    Time: 334 ms (69.74%), Space: 29.7 MB (20.9%) - LeetHub

--- 1891-cutting-ribbons/1891-cutting-ribbons.py
+++ 1891-cutting-ribbons/1891-cutting-ribbons.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxLength(self, ribbons: List[int], k: int) -> int:
         left = 1 
-        right = max(ribbons) + 1
+        right = max(ribbons)
         mid = (left + right) // 2
         ans = 0
-        while left < right:
+        while left <= right:
             count = 0
             for r in ribbons:
                 count += r // mid
@@ -13,6 +13,6 @@ class Solution:
                 left = mid + 1
                 mid = (left + right) // 2
             else:
-                right = mid
+                right = mid - 1
                 mid = (left + right) // 2
         return ans 
