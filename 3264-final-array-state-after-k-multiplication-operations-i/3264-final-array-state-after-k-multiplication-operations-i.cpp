516009940b231b95448f7f9f22386db49bb4dfcc
KangwooChoi <mickeychoi83@gmail.com>
2024-12-16 13:59:08 +0900

    Time: 0 ms (100%), Space: 27.7 MB (29.75%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        priority_queue<pair<int, int>> pq;
+        vector<pair<int, int>> heap;
         for (int i = 0; i < nums.size(); i++) {
-            pq.push({-nums[i], -i});
+            heap.push_back({nums[i], i});
         }
-        pair<int, int> curr;
-        int val = 0, index = 0;
-        while(k > 0) {
-            curr = pq.top();
-            val = -curr.first;
-            index = -curr.second;
-            nums[index] = val * multiplier;
-            pq.pop();
-            pq.push({-val*multiplier, -index});
-            k--;
+        make_heap(heap.begin(), heap.end(), greater<>());
+        while(k--) {
+            pop_heap(heap.begin(), heap.end(), greater<>());
+            auto [value, i] = heap.back();
+            heap.pop_back();
+
+            nums[i] *= multiplier;
+            heap.push_back({nums[i], i});
+            push_heap(heap.begin(), heap.end(), greater<>());
         }
-        return nums; 
+        return nums;
     }
 };
+//class Solution {
+//public:
+//    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+//        priority_queue<pair<int, int>> pq;
+//        for (int i = 0; i < nums.size(); i++) {
+//            pq.push({-nums[i], -i});
+//        }
+//        pair<int, int> curr;
+//        int val = 0, index = 0;
+//        while(k > 0) {
+//            curr = pq.top();
+//            val = -curr.first;
+//            index = -curr.second;
+//            nums[index] = val * multiplier;
+//            pq.pop();
+//            pq.push({-val*multiplier, -index});
+//            k--;
+//        }
+//        return nums; 
+//    }
+//};
