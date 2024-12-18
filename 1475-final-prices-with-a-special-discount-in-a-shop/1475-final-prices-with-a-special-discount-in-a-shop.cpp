d01551ff078d24b845c5aed4c83874d20a4550c7
KangwooChoi <mickeychoi83@gmail.com>
2024-12-18 11:36:02 +0900

    Time: 0 ms (100%), Space: 14.1 MB (24.39%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        int n = prices.size();
-        vector<int> answer = prices;
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                if (prices[i] >= prices[j]) {
-                    answer[i] = prices[i] - prices[j];
-                    break;
-                }
+        vector<int> ans = prices;
+        stack<int> stack;
+
+        for (int i = 0; i < prices.size(); i++) {
+            while (!stack.empty() && prices[stack.top()] >= prices[i]) {
+                ans[stack.top()] -= prices[i];
+                stack.pop();
             }
+            stack.push(i);
         }
-        return answer;        
+        return ans;
     }
 };
+//class Solution {
+//public:
+//    vector<int> finalPrices(vector<int>& prices) {
+//        int n = prices.size();
+//        vector<int> answer = prices;
+//        for (int i = 0; i < n; i++) {
+//            for (int j = i+1; j < n; j++) {
+//                if (prices[i] >= prices[j]) {
+//                    answer[i] = prices[i] - prices[j];
+//                    break;
+//                }
+//            }
+//        }
+//        return answer;        
+//    }
+//};
