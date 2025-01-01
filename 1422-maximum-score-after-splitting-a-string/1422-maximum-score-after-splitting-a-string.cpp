f58994cb0f619099804d7f0f2b9f8ae3a7e8b3b9
KangwooChoi <mickeychoi83@gmail.com>
2025-01-01 11:13:31 +0900

    Time: 0 ms (100%), Space: 9.3 MB (15.33%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -7,7 +7,6 @@ public:
         int left = 0;
         int right = 0;
         if (s[0] == '1') freq[0] = 1;
-        int curr = 1;
         for (int i = 1; i < n; i++) {
             if (s[i] == '1') {
                 freq[i] = freq[i-1] + 1;
@@ -17,9 +16,7 @@ public:
         }
         int one_total = freq[n-1];
         for (int i = 0; i < n - 1; i++) {
-            right = one_total - freq[i];
-            left = i + 1 - freq[i];
-            ans = max(ans, left + right);
+            ans = max(ans, one_total + i + 1 - 2*freq[i]);
         }
         return ans; 
     }
