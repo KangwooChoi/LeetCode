af4424caf082c25cc4deda98aaebab8c862335f8
KangwooChoi <mickeychoi83@gmail.com>
2025-01-06 11:08:43 +0900

    Time: 1 ms (68.94%), Space: 12.1 MB (67.01%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,15 +2,38 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n = boxes.length();
-        vector<int> ans(n, 0); 
+        vector<int> ans(n,0);
+        
+        int ballsToLeft = 0, movesToLeft = 0;
+        int ballsToRight = 0, movesToRight = 0;
+        
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (boxes[j] == '1') {
-                    //ans[i] += (j > i) ? j - i : i - j;
-                    ans[i] += abs(j-i);
-                }
-            }
+            ans[i] += movesToLeft;
+            ballsToLeft += boxes[i] - '0';
+            movesToLeft += ballsToLeft;
+
+            int j = n - 1 - i;
+            ans[j] += movesToRight;
+            ballsToRight += boxes[j] - '0';
+            movesToRight += ballsToRight;
+
         }
         return ans;
     }
 };
+//class Solution {
+//public:
+//    vector<int> minOperations(string boxes) {
+//        int n = boxes.length();
+//        vector<int> ans(n, 0); 
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (boxes[j] == '1') {
+//                    //ans[i] += (j > i) ? j - i : i - j;
+//                    ans[i] += abs(j-i);
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+//};
