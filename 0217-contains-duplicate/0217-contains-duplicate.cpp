89f5fb696334fc874709d4b8096da7b491aa77f6
KangwooChoi <mickeychoi83@gmail.com>
2025-01-28 18:17:32 +0900

    Time: 73 ms (6.99%), Space: 74.6 MB (26.68%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        if (nums.size() < 2) return false;
-        sort(nums.begin(), nums.end());
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i-1] == nums[i]) return true;
-        }
+        set<int> s;
+        for (int i = 0; i < nums.size(); i++) {
+            if (s.find(nums[i]) != s.end()) return true;
+            s.insert(nums[i]);
+        } 
         return false;
     }
 };
