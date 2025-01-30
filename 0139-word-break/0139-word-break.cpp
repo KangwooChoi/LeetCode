a0889eb1941e78f3a3979923351c729bd69f70a0
KangwooChoi <mickeychoi83@gmail.com>
2025-01-30 18:24:10 +0900

    Time: 11 ms (47.85%), Space: 16.1 MB (42.68%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> words(wordDict.begin(), wordDict.end());
+        queue<int> queue;
+        vector<bool> seen(s.length(), false);
+        queue.push(0);
+
+        while (!queue.empty()) {
+            int start = queue.front();
+            queue.pop();
+
+            if (start == s.length()) {
+                return true;
+            }
+
+            for (int end = start + 1; end <= s.length(); end++) {
+                if (seen[end]) {
+                    continue;
+                }
+
+                if (words.find(s.substr(start, end - start)) != words.end()) {
+                    queue.push(end);
+                    seen[end] = true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+//class Solution {
+//public:
+//    bool wordBreak(string s, vector<string>& wordDict) {
+//        int n = wordDict.size();
+//        reverse(wordDict.begin(), wordDict.end());
+//        for (int i = 0; i < n; i++) {
+//            int length = wordDict[i].length();
+//            if (s.substr(0, length) == wordDict[i]) {
+//                bool ret = backtrack(s.substr(length), wordDict);
+//                if (ret) return true;
+//            }
+//        }
+//        return false; 
+//    }
+//private:
+//    bool backtrack(string s, vector<string>& wordDict) {
+//        if (s == "") return true;
+//        for (int i = 0; i < wordDict.size(); i++) {
+//            int length = wordDict[i].length();
+//            if (s.substr(0, length) == wordDict[i]) {
+//                bool ret = backtrack(s.substr(length), wordDict);
+//                if (ret) return true;
+//            }
+//        }
+//        return false;
+//    }
+//};
