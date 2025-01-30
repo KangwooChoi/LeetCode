de3dd6009e4a1cc7271d9978edb159bb2a0bc411
KangwooChoi <mickeychoi83@gmail.com>
2025-01-30 18:40:05 +0900

    Time: 0 ms (100%), Space: 10.2 MB (95.29%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> words(wordDict.begin(), wordDict.end());
-        queue<int> queue;
-        vector<bool> seen(s.length(), false);
-        queue.push(0);
+        vector<bool> dp(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            for (string word : wordDict) {
+                if (i < word.length()-1) continue;
 
-        while (!queue.empty()) {
-            int start = queue.front();
-            queue.pop();
-
-            if (start == s.length()) {
-                return true;
-            }
-
-            for (int end = start + 1; end <= s.length(); end++) {
-                if (seen[end]) {
-                    continue;
+                if (i == word.length()-1 || dp[i-word.length()]) {
+                    if (s.substr(i-word.length()+1, word.length()) == word) {
+                        dp[i] = true;
+                        break;
+                    }
                 }
 
-                if (words.find(s.substr(start, end - start)) != words.end()) {
-                    queue.push(end);
-                    seen[end] = true;
-                }
             }
         }
-
-        return false;
+        return dp[s.length()-1];
     }
 };
+//class Solution {
+//public:
+//    bool wordBreak(string s, vector<string>& wordDict) {
+//        unordered_set<string> words(wordDict.begin(), wordDict.end());
+//        queue<int> queue;
+//        vector<bool> seen(s.length(), false);
+//        queue.push(0);
+//
+//        while (!queue.empty()) {
+//            int start = queue.front();
+//            queue.pop();
+//
+//            if (start == s.length()) {
+//                return true;
+//            }
+//
+//            for (int end = start + 1; end <= s.length(); end++) {
+//                if (seen[end]) {
+//                    continue;
+//                }
+//
+//                if (words.find(s.substr(start, end - start)) != words.end()) {
+//                    queue.push(end);
+//                    seen[end] = true;
+//                }
+//            }
+//        }
+//
+//        return false;
+//    }
+//};
 //class Solution {
 //public:
 //    bool wordBreak(string s, vector<string>& wordDict) {
