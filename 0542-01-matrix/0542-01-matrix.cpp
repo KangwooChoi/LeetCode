e37e3c849c5a7b41364c0cfb18913324a89b4519
KangwooChoi <mickeychoi83@gmail.com>
2025-02-01 19:36:45 +0900

    Time: 10 ms (87.49%), Space: 32.6 MB (95.22%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,42 +1,52 @@
 class Solution {
 public:
-    int m;
-    int n;
-    vector<vector<int>> dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        vector<vector<int>> ans(m, vector<int>(n,0));
-        vector<vector<bool>> seen(m, vector<bool>(n,false));
-        queue<vector<int>> q;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                ans[i][j] = mat[i][j];
-                if(ans[i][j] == 0) {
-                    seen[i][j] = true;
-                    q.push({i,j,0});
+        vector<vector<int>> dp;
+        
+        for (vector<int>& row: mat) {
+            dp.emplace_back(row.begin(), row.end());
+        }
+        
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (dp[row][col] == 0) {
+                    continue;
                 }
+
+                int minNeighbor = m * n;
+                if (row > 0) {
+                    minNeighbor = min(minNeighbor, dp[row - 1][col]);
+                }
+                
+                if (col > 0) {
+                    minNeighbor = min(minNeighbor, dp[row][col - 1]);
+                }
+                
+                dp[row][col] = minNeighbor + 1;
             }
         }
-
-        while (!q.empty()) {
-            vector<int> curr = q.front();
-            q.pop();
-            int row = curr[0], col = curr[1], step = curr[2];
-            for (auto dir : dirs) {
-                int nextRow = row + dir[0], nextCol = col + dir[1];
-                if (valid(nextRow, nextCol, m, n) && !seen[nextRow][nextCol]) {
-                    seen[nextRow][nextCol] = true;
-                    q.push({nextRow, nextCol, step+1});
-                    ans[nextRow][nextCol] = step+1; 
+        
+        for (int row = m - 1; row >= 0; row--) {
+            for (int col = n - 1; col >= 0; col--) {
+                if (dp[row][col] == 0) {
+                    continue;
                 }
+                
+                int minNeighbor = m * n;
+                if (row < m - 1) {
+                    minNeighbor = min(minNeighbor, dp[row + 1][col]);
+                }
+                
+                if (col < n - 1) {
+                    minNeighbor = min(minNeighbor, dp[row][col + 1]);
+                }
+                
+                dp[row][col] = min(dp[row][col], minNeighbor + 1);
             }
         }
-        return ans; 
-    }
-    bool valid(int x, int y, int m, int n) {
-        return (0<= x && x < m && 0 <= y && y < n);
+        
+        return dp;
     }
 };
