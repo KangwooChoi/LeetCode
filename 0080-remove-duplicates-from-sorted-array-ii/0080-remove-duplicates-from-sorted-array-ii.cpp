acd0142d37ce0d8e77bb0492d3e9106371a35587
KangwooChoi <mickeychoi83@gmail.com>
2025-02-02 21:24:31 +0900

    Time: 11 ms (6.77%), Space: 19.7 MB (6.14%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,21 +1,51 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n = nums.size();
-        if (n <= 2) return n;
-        int prev = nums[0];
-        int cnt = 1;
-        int p = 1; 
-        for (int i = 1; i < n; i++) {
-            if (nums[i] != nums[i-1]) {
-                nums[p++] = nums[i];
-                cnt = 1;
-            } else {
-                if(++cnt <= 2) {
-                    nums[p++] = nums[i];
-                } 
-            }
+        if (nums.empty()) {
+            return 0;
         }
-        return p;
+
+        int i = 1;      // Pointer to iterate through the array
+        int j = 1;      // Pointer to track position for valid elements
+        int count = 1;  // Count of occurrences of the current element
+
+        while (i < nums.size()) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+                if (count > 2) {
+                    i++;
+                    continue;
+                }
+            } else {
+                count = 1;
+            }
+            nums[j] = nums[i];
+            j++;
+            i++;
+        }
+
+        nums.resize(j);
+        return j;
     }
 };
+//class Solution {
+//public:
+//    int removeDuplicates(vector<int>& nums) {
+//        int n = nums.size();
+//        if (n <= 2) return n;
+//        int prev = nums[0];
+//        int cnt = 1;
+//        int p = 1; 
+//        for (int i = 1; i < n; i++) {
+//            if (nums[i] != nums[i-1]) {
+//                nums[p++] = nums[i];
+//                cnt = 1;
+//            } else {
+//                if(++cnt <= 2) {
+//                    nums[p++] = nums[i];
+//                } 
+//            }
+//        }
+//        return p;
+//    }
+//};
