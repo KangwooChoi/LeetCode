dbe3762d2490cc31ca182e59641d59fa534a7812
KangwooChoi <mickeychoi83@gmail.com>
2025-02-05 13:10:53 +0900

    Time: 0 ms (100%), Space: 17.7 MB (83.98%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int ans = nums[0];
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            int mid = nums[i];
-            ans = max(ans, mid);
-            for (int j = i + 1; j < n; j++) {
-                mid *= nums[j];
-                ans = max(ans, mid);
-                if (nums[j] == 0) break;
-            }
+        if (nums.size() == 0) return 0;
+
+        int max_so_far = nums[0];
+        int min_so_far = nums[0];
+        int result = max_so_far;
+
+        for (int i = 1; i < nums.size(); i++) {
+            int curr = nums[i];
+            int temp_max = max(curr, max(max_so_far * curr, min_so_far * curr));
+            min_so_far = min(curr, min(max_so_far * curr, min_so_far * curr));
+
+            // Update max_so_far after updates to min_so_far to avoid
+            // overwriting it
+            max_so_far = temp_max;
+            // Update the result with the maximum product found so far
+            result = max(max_so_far, result);
         }
-        return ans; 
+
+        return result;
     }
 };
+//class Solution {
+//public:
+//    int maxProduct(vector<int>& nums) {
+//        int min_so_far = nums[0];
+//        int max_so_far = nums[0];
+//        int result = nums[0];
+//        for (int i = 1; i < nums.size(); i++) {
+//            max_so_far = max({nums[i], min_so_far*nums[i], max_so_far*nums[i]});
+//            min_so_far = min({nums[i], min_so_far*nums[i], max_so_far*nums[i]});
+//            result = max(result, max_so_far);
+//        }
+//        return result; 
+//    }
+//};
