9edf155a3c97a287f774342a1e45512b2371bb7d
KangwooChoi <mickeychoi83@gmail.com>
2025-02-10 16:34:28 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,13 +2,31 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int ans = 0;
-        for (int start = 0; start < nums.size(); start++) {
-            int midSum = 0;
-            for (int end = start; end < nums.size(); end++) {
-                midSum += nums[end];
-                if (midSum == k) ans++;
-            } 
+        unordered_map<int,int> umap;
+        umap.insert({0,1});
+        int midSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            midSum += nums[i];
+            umap[midSum]++;
+            int key = midSum - k;
+            if (umap.find(key) != umap.end()) {
+                ans += umap[key];
+            }
         }
-        return ans; 
+        return ans;
     }
 };
+//class Solution {
+//public:
+//    int subarraySum(vector<int>& nums, int k) {
+//        int ans = 0;
+//        for (int start = 0; start < nums.size(); start++) {
+//            int midSum = 0;
+//            for (int end = start; end < nums.size(); end++) {
+//                midSum += nums[end];
+//                if (midSum == k) ans++;
+//            } 
+//        }
+//        return ans; 
+//    }
+//};
