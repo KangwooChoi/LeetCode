23a98460a860f4a7a0e6ace73db2033f26005fae
KangwooChoi <mickeychoi83@gmail.com>
2025-02-17 12:27:47 +0900

    Time: 36 ms (40.21%), Space: 17.9 MB (37.37%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numTilePossibilities(string tiles) {
+        unordered_set<string> seq;
+        vector<bool> used(tiles.length(), false);
+
+        generateSeq(tiles, "", used, seq);
+    
+        return seq.size() - 1;     
+    }
+
+private:
+    void generateSeq(string& tiles, string curr, vector<bool>& used, unordered_set<string>& seq) {
+        seq.insert(curr);
+        for (int pos = 0; pos < tiles.length(); pos++) {
+            if (!used[pos]) {
+                used[pos] = true;
+                generateSeq(tiles, curr + tiles[pos], used, seq);
+                used[pos] = false;
+            }
+        }
+    }
+};
