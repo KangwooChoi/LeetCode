3f1fec8b474ccb54137f3ef681ccab4abb6f203f
KangwooChoi <mickeychoi83@gmail.com>
2025-02-26 22:12:18 +0900

    Time: 0 ms (100%), Space: 8.4 MB (33.01%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        vector<char> diff_s1;
-        vector<char> diff_s2;
-        int n = s1.length();
-        for (int i = 0; i < n; i++) {
+        stack<char> st;
+        bool once = false;
+        for (int i = 0; i < s1.length(); i++) {
             if (s1[i] != s2[i]) {
-                diff_s1.push_back(s1[i]);
-                diff_s2.push_back(s2[i]);
-                if (diff_s1.size() > 2) return false;
-            }
+                if (st.empty() && !once) {
+                    st.push(s1[i]);
+                    st.push(s2[i]);
+                } else {
+                    if (once) return false;
+                    once = true;
+                    if (st.top() != s1[i]) return false;
+                    st.pop();
+                    if (st.top() != s2[i]) return false;
+                    st.pop();
+                }
+            } 
         }
-        if (diff_s1.size() == 0) return true;
-        else if (diff_s1.size() == 2) {
-            if (diff_s1[0] == diff_s2[1] && diff_s1[1] == diff_s2[0]) return true;
-        }
-        return false;
+        if (!st.empty()) return false;
+        return true;
     }
 };
