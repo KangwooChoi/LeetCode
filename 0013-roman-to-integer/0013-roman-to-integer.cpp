f7526ad63af7d82b59a8afff26b66568590444d3
KangwooChoi <mickeychoi83@gmail.com>
2025-02-26 18:31:23 +0900

    Time: 5 ms (58.72%), Space: 13.4 MB (35.66%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int p = 0;
+        map<char,int> values{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
         int ans = 0;
-        s += ' ';
-        while (p < s.length()) {
-            if (s[p] == 'M') ans += 1000;
-            else if (s[p] == 'D') ans += 500;
-            else if (s[p] == 'C') {
-                if (s[p+1] == 'M') {
-                    ans += 900;
-                    p++;
-                } else if (s[p+1] == 'D') {
-                    ans += 400;
-                    p++;
-                } else {
-                    ans += 100;
-                }
+        int i = 0;
+        while (i < s.length()) {
+            char currChar = s[i];
+            int currVal = values[currChar];
+            int nextVal = 0;
+            if (i+1 < s.length()) {
+                char nextChar = s[i+1];
+                nextVal = values[nextChar];
             }
-            else if (s[p] == 'L') ans += 50;
-            else if (s[p] == 'X') {
-                if (s[p+1] == 'C') {
-                    ans += 90;
-                    p++;
-                } else if (s[p+1] == 'L') {
-                    ans += 40;
-                    p++;
-                } else {
-                    ans += 10;
-                } 
-            } else if (s[p] == 'V') ans += 5;
-            else if (s[p] == 'I') {
-                if (s[p+1] == 'X') {
-                    ans += 9; 
-                    p++;
-                } else if (s[p+1] == 'V') {
-                    ans += 4;
-                    p++;
-                } else {
-                    ans += 1;
-                }
-            } 
-            p++;
+            if (currVal < nextVal) {
+                ans += nextVal - currVal;
+                i += 2;
+            } else {
+                ans += currVal;
+                i += 1;
+            }
         } 
         return ans;
     }
