42e99143a04c8c95f9dac146b28085f9051ba017
KangwooChoi <mickeychoi83@gmail.com>
2025-02-26 22:35:29 +0900

    Time: 0 ms (100%), Space: 23.9 MB (17.99%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,10 +3,8 @@ public:
     void moveZeroes(vector<int>& nums) {
         int p = 0;
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[p++] = nums[i];
-            }
-        } 
+            if (nums[i] != 0) nums[p++] = nums[i];
+        }
         while (p < nums.size()) {
             nums[p++] = 0;
         }
