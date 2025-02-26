8b2fe82a25cb579d12334e0a2e13019680c2a3b8
KangwooChoi <mickeychoi83@gmail.com>
2025-02-26 17:07:22 +0900

    Time: 1 ms (38.61%), Space: 28.2 MB (24.52%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int half = nums.size() / 2; 
-        if (nums.size() % 2 != 0) half++; 
-        int contiguous = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i-1] == nums[i]) contiguous++; 
-            else contiguous = 1;
-            if (contiguous >= half) return nums[i];
+        int n = nums.size();
+        unordered_map<int,int> umap;
+        for (int i = 0; i < n; i++) {
+            umap[nums[i]]++;
         }
-        return -1;
+        for (auto [num, freq] : umap) {
+            if (freq > n / 2) return num;
+        }
+        return -1; 
     }
 };
