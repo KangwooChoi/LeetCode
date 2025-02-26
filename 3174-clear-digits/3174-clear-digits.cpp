a1ac2bb3b95e26680d9a39bd3139cf7b8c3e6a6b
KangwooChoi <mickeychoi83@gmail.com>
2025-02-26 22:32:18 +0900

    Time: 0 ms (100%), Space: 8.5 MB (60.53%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        string ans = "";
+        string ans = ""; 
         for (char c : s) {
-            if (isDigit(c)) ans = ans.substr(0,ans.size()-1);
+            if (isdigit(c)) ans.pop_back();
             else ans.push_back(c);
         }
-        return ans; 
-    }
-    bool isDigit(char c) {
-        return c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9';
+        return ans;
     }
 };
