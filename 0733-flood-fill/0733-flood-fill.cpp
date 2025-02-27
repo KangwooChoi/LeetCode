b25da5fa60ff39c7f477760726b276db62011e77
KangwooChoi <mickeychoi83@gmail.com>
2025-02-27 19:55:32 +0900

    Time: 0 ms (100%), Space: 18.4 MB (35.16%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int dx[4] = {1, -1, 0, 0};
-    int dy[4] = {0, 0, 1, -1};
+    vector<int> dx = {1, -1, 0, 0};
+    vector<int> dy = {0, 0, 1, -1};
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m = image.size(), n = image[0].size();
-        queue<pair<int,int>> q;
         if (image[sr][sc] == color) return image;
-        int prevColor = image[sr][sc];
-        q.push({sr,sc}); 
+        queue<pair<int,int>> q; 
+        int origin = image[sr][sc];
         image[sr][sc] = color;
-        while(!q.empty()) {
+        q.push({sr,sc});
+        while (!q.empty()) {
             int size = q.size();
             for (int i = 0; i < size; i++) {
-                auto [row, col] = q.front();
+                auto curr = q.front();
                 q.pop();
                 for (int k = 0; k < 4; k++) {
-                    int nx = row + dx[k];
-                    int ny = col + dy[k];
-                    if (0 <= nx && nx < m && 0 <= ny && ny < n && image[nx][ny] == prevColor) {
-                        q.push({nx,ny});
-                        image[nx][ny] = color;
+                    int nx = curr.first + dx[k]; 
+                    int ny = curr.second + dy[k]; 
+                    if (0 <= nx && nx < image.size() && 0 <= ny && ny < image[0].size()) {
+                        if (image[nx][ny] == origin) {
+                            image[nx][ny] = color;
+                            q.push({nx,ny});
+                        }
                     }
                 }
             }
-        }
+        } 
         return image;
     }
 };
