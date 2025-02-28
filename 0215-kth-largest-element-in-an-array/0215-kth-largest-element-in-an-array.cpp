665f268fe2c65fb12f2e546b483da28822bfc810
KangwooChoi <mickeychoi83@gmail.com>
2025-02-28 17:43:55 +0900

    Time: 37 ms (37.94%), Space: 61.4 MB (58.96%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,14 +1,52 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            pq.push(nums[i]);
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (auto num : nums) {
+            pq.push(num);
+            if (pq.size() > k) {
+                pq.pop();
+            }
         }
-        while (--k) {
-            pq.pop(); 
-        }
-        return pq.top(); 
+        return pq.top();
     }
 };
+
+//class Solution {
+//public:
+//    int findKthLargest(vector<int>& nums, int k) {
+//        sort(nums.begin(), nums.end(), greater<int>());
+//        return nums[k-1];
+//    }
+//
+//};
+
+//class Solution {
+//public:
+//    int findKthLargest(vector<int>& nums, int k) {
+//        priority_queue<int> heap;
+//        for (int num: nums) {
+//            heap.push(-num);
+//            if (heap.size() > k) {
+//                heap.pop();
+//            }
+//        }
+//        
+//        return -heap.top();
+//    }
+//};
+
+//class Solution {
+//public:
+//    int findKthLargest(vector<int>& nums, int k) {
+//        priority_queue<int> pq;
+//        int n = nums.size();
+//        for (int i = 0; i < n; i++) {
+//            pq.push(nums[i]);
+//        }
+//        while (--k) {
+//            pq.pop(); 
+//        }
+//        return pq.top(); 
+//    }
+//};
