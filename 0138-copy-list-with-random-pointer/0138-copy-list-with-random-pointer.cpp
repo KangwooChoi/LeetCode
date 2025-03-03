5c9aec858586600b64f3cbeb6917591ca52d4e51
KangwooChoi <mickeychoi83@gmail.com>
2025-03-03 19:48:02 +0900

    Time: 0 ms (100%), Space: 15.4 MB (9.99%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -13,24 +13,42 @@ public:
     }
 };
 */
-
 class Solution {
 private:
     unordered_map<Node*,Node*> visitedHash;
 
 public:
     Node* copyRandomList(Node* head) {
-        if (head == NULL) {
-            return NULL;
+        if (head == nullptr) {
+            return nullptr;
+        }        
+        if (visitedHash.find(head) != visitedHash.end()) {
+            return visitedHash[head];
         }
-        if (this->visitedHash.find(head) != this->visitedHash.end()) {
-            return this->visitedHash[head];
-        }
-        Node* node = new Node(head->val, NULL, NULL);
-        this->visitedHash[head] = node;
-
-        node->next = this->copyRandomList(head->next);
-        node->random = this->copyRandomList(head->random); 
+        Node* node = new Node(head->val, nullptr, nullptr);
+        visitedHash[head] = node;
+        node->next = copyRandomList(head->next);
+        node->random = copyRandomList(head->random);
         return node;
     }
 };
+//class Solution {
+//private:
+//    unordered_map<Node*,Node*> visitedHash;
+//
+//public:
+//    Node* copyRandomList(Node* head) {
+//        if (head == NULL) {
+//            return NULL;
+//        }
+//        if (this->visitedHash.find(head) != this->visitedHash.end()) {
+//            return this->visitedHash[head];
+//        }
+//        Node* node = new Node(head->val, NULL, NULL);
+//        this->visitedHash[head] = node;
+//
+//        node->next = this->copyRandomList(head->next);
+//        node->random = this->copyRandomList(head->random); 
+//        return node;
+//    }
+//};
