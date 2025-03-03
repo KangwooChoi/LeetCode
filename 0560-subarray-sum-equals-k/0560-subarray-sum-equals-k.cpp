1b5ac789841c9b3a2fc81ce5a4f9e86654aa8de9
KangwooChoi <mickeychoi83@gmail.com>
2025-03-03 11:14:49 +0900

    Time: 39 ms (79.27%), Space: 45.4 MB (67.62%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> partSum(n+1, 0);
-        for (int i = 0; i < n; i++) {
-            partSum[i+1] = partSum[i] + nums[i];
-        } 
-        int left = 0;
+        unordered_map<int,int> umap;
+        umap[0] = 1;
         int ans = 0;
-        for (int left = 0; left < n + 1; left++) {
-            for (int right = left + 1; right < n+1; right++) {
-                if (partSum[right] - partSum[left] == k) ans++;
+        int sum = 0;
+        for (auto num : nums) {
+            sum += num;
+            if (umap.find(sum-k) != umap.end()) {
+                ans += umap[sum-k];
             }
+            umap[sum]++;
         }
         return ans;
     }
 };
+//class Solution {
+//public:
+//    int subarraySum(vector<int>& nums, int k) {
+//        int n = nums.size();
+//        vector<int> partSum(n+1, 0);
+//        for (int i = 0; i < n; i++) {
+//            partSum[i+1] = partSum[i] + nums[i];
+//        } 
+//        int left = 0;
+//        int ans = 0;
+//        for (int left = 0; left < n + 1; left++) {
+//            for (int right = left + 1; right < n+1; right++) {
+//                if (partSum[right] - partSum[left] == k) ans++;
+//            }
+//        }
+//        return ans;
+//    }
+//};
