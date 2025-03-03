b4085ae1653d6e6b7af5ab1950d056110fb3ee6a
KangwooChoi <mickeychoi83@gmail.com>
2025-03-03 19:20:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,18 +4,36 @@ public:
         vector<vector<int>> ans;
         sort(intervals.begin(), intervals.end());
         for (auto interval : intervals) {
-            if (ans.empty()) {
-                ans.push_back(interval);
-                continue;
-            }
-            if (ans.back()[1] < interval[0]) {
-                ans.push_back(interval);
-                continue;
-            }
-            if (interval[0] <= ans.back()[1] && ans.back()[1] < interval[1]) {
-                ans.back()[1] = interval[1];
+            if (ans.empty()) ans.push_back(interval);
+            else {
+                if (ans.back()[1] >= interval[0]) {
+                    ans.back()[1] = interval[1];
+                } else {
+                    ans.push_back(interval);
+                }
             }
         }
         return ans;
     }
 };
+//class Solution {
+//public:
+//    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//        vector<vector<int>> ans;
+//        sort(intervals.begin(), intervals.end());
+//        for (auto interval : intervals) {
+//            if (ans.empty()) {
+//                ans.push_back(interval);
+//                continue;
+//            }
+//            if (ans.back()[1] < interval[0]) {
+//                ans.push_back(interval);
+//                continue;
+//            }
+//            if (interval[0] <= ans.back()[1] && ans.back()[1] < interval[1]) {
+//                ans.back()[1] = interval[1];
+//            }
+//        }
+//        return ans;
+//    }
+//};
