4e12c69eeb96cc2b5781d0a3f386db9ad81866b9
KangwooChoi <mickeychoi83@gmail.com>
2025-03-05 09:12:21 +0900

    Time: 0 ms (100%), Space: 7.9 MB (31.17%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        vector<long long> dp(n+1,0);
-        dp[1] = 1;
-        for (int i = 1; i < n; i++) {
-            dp[i+1] = dp[i] + i*4;
-        }
-        return dp[n]; 
+        return 1 + (long long) n * (n-1) * 2;
     }
 };
+//class Solution {
+//public:
+//    long long coloredCells(int n) {
+//        vector<long long> dp(n+1,0);
+//        dp[1] = 1;
+//        for (int i = 1; i < n; i++) {
+//            dp[i+1] = dp[i] + i*4;
+//        }
+//        return dp[n]; 
+//    }
+//};
