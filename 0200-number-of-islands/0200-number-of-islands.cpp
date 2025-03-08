a76e5f5f1cab767bd18c70f49471a50ef9adaec3
KangwooChoi <mickeychoi83@gmail.com>
2025-03-08 13:57:41 +0900

    Time: 18 ms (97.53%), Space: 23 MB (35.42%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,74 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(),false));
-        vector<int> dx = {1, -1, 0, 0};
-        vector<int> dy = {0, 0, 1, -1};
+  int numIslands(vector<vector<char>>& grid) {
+    int nr = grid.size();
+    if (!nr) return 0;
+    int nc = grid[0].size();
 
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    queue<pair<int,int>> q;
-                    q.push({i,j});    
-                    visited[i][j] = true;
-                    ans++;
-                    while (!q.empty()){
-                        int size = q.size();
-                        for (int l = 0; l < size; l++) {
-                            pair<int,int> curr = q.front();
-                            q.pop();
-                            for (int k = 0; k < 4; k++) {
-                                int nx = curr.first + dx[k];
-                                int ny = curr.second + dy[k];
-                                if (0<= nx && nx < grid.size() && 0 <= ny && ny < grid[0].size() && visited[nx][ny] == false && grid[nx][ny] == '1') {
-                                    q.push({nx,ny});
-                                    visited[nx][ny] = true;
-                                }
-                            }
-                        }
-                    }
-                }
+    int num_islands = 0;
+    for (int r = 0; r < nr; ++r) {
+      for (int c = 0; c < nc; ++c) {
+        if (grid[r][c] == '1') {
+          ++num_islands;
+          grid[r][c] = '0'; // mark as visited
+          queue<pair<int, int>> neighbors;
+          neighbors.push({r, c});
+          while (!neighbors.empty()) {
+            auto rc = neighbors.front();
+            neighbors.pop();
+            int row = rc.first, col = rc.second;
+            if (row - 1 >= 0 && grid[row-1][col] == '1') {
+              neighbors.push({row-1, col}); grid[row-1][col] = '0';
             }
+            if (row + 1 < nr && grid[row+1][col] == '1') {
+              neighbors.push({row+1, col}); grid[row+1][col] = '0';
+            }
+            if (col - 1 >= 0 && grid[row][col-1] == '1') {
+              neighbors.push({row, col-1}); grid[row][col-1] = '0';
+            }
+            if (col + 1 < nc && grid[row][col+1] == '1') {
+              neighbors.push({row, col+1}); grid[row][col+1] = '0';
+            }
+          }
         }
-        return ans; 
+      }
     }
+
+    return num_islands;
+  }
 };
+//class Solution {
+//public:
+//    int dx[4] = {1, -1, 0, 0};
+//    int dy[4] = {0, 0, 1, -1};
+//
+//    int numIslands(vector<vector<char>>& grid) {
+//        int ans = 0;
+//        int m = grid.size(), n = grid[0].size();
+//        queue<pair<int,int>> q;
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (grid[i][j] == '1') {
+//                    ans++;
+//                    q.push({i,j});
+//                    while (!q.empty()) {
+//                        int size = q.size();
+//                        for (int s = 0; s < size; s++) {
+//                            auto curr = q.front();
+//                            grid[curr.first][curr.second] = '2';
+//                            q.pop();
+//                            for (int k = 0; k < 4; k++) {
+//                                int nx = curr.first + dx[k];
+//                                int ny = curr.second + dy[k];
+//                                if (0 <= nx && nx < m && 0 <= ny && ny < n && grid[nx][ny] == '1') {
+//                                    q.push({nx,ny});
+//                                }
+//                            }
+//                        }
+//                    }
+//                } 
+//            }
+//        }
+//        return ans; 
+//    }
+//};
