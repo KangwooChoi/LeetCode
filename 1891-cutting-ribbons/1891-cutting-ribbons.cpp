18f8b83fc3b61cddf9e1ebc065e908ff87a995da
KangwooChoi <mickeychoi83@gmail.com>
2025-03-09 15:03:15 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1891-cutting-ribbons/1891-cutting-ribbons.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxLength(vector<int>& ribbons, int k) {
+        int left = 0, right = 0;
+        for (int i = 0; i < ribbons.size(); i++) {
+            right = max(right, ribbons[i]);
+        }
+        int ans = 0;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int cnt = 0;
+            for (int i = 0; i < ribbons.size(); i++) {
+                cnt += ribbons[i] / mid;
+            }
+            if (cnt >= k) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans; 
+    }
+};
