75d82fb330e347aba0316034b30eeeebdaf7322c
KangwooChoi <mickeychoi83@gmail.com>
2025-03-11 12:52:38 +0900

    Time: 15 ms (25.48%), Space: 17.9 MB (29.93%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -5,27 +5,58 @@ public:
         vector<vector<int>> ans;
         int n = candidates.size();
         for (int i = 0; i < n; i++) {
-            vector<int> result;
-            int cand = candidates[i];
-            result.push_back(cand);
-            backtrack(candidates, i, target-cand, result, ans);
-            result.pop_back();
-        } 
+            vector<int> inter;
+            int candidate = candidates[i];
+            inter.push_back(candidate);
+            backtrack(candidates, i, target-candidate, inter, ans);
+            inter.pop_back();
+        }
         return ans;
     }
 private:
-    void backtrack(vector<int>& candidates, int index, int target, vector<int> res, vector<vector<int>>& ans) {
+    void backtrack(vector<int>& candidates, int start, int target, vector<int> inter, vector<vector<int>>& ans) {
+        if (target < 0) return;
         if (target == 0) {
-            ans.push_back(res);
+            ans.push_back(inter);
             return;
         }
-        if (target < 0) return;
-        for (int i = index; i < candidates.size(); i++) {
-            int cand = candidates[i];
-            res.push_back(cand);
-            backtrack(candidates, i, target-cand, res, ans);
-            res.pop_back();
+        for (int i = start; i < candidates.size(); i++) {
+            int candidate = candidates[i];
+            inter.push_back(candidate);
+            backtrack(candidates, i, target-candidate, inter, ans);
+            inter.pop_back();
         }
         return;
     }
 };
+//class Solution {
+//public:
+//    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//        sort(candidates.rbegin(), candidates.rend());
+//        vector<vector<int>> ans;
+//        int n = candidates.size();
+//        for (int i = 0; i < n; i++) {
+//            vector<int> result;
+//            int cand = candidates[i];
+//            result.push_back(cand);
+//            backtrack(candidates, i, target-cand, result, ans);
+//            result.pop_back();
+//        } 
+//        return ans;
+//    }
+//private:
+//    void backtrack(vector<int>& candidates, int index, int target, vector<int> res, vector<vector<int>>& ans) {
+//        if (target == 0) {
+//            ans.push_back(res);
+//            return;
+//        }
+//        if (target < 0) return;
+//        for (int i = index; i < candidates.size(); i++) {
+//            int cand = candidates[i];
+//            res.push_back(cand);
+//            backtrack(candidates, i, target-cand, res, ans);
+//            res.pop_back();
+//        }
+//        return;
+//    }
+//};
