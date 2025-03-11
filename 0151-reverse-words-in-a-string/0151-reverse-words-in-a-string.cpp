a6c09521b7f498bd7eccb047c122c26cc25fe0f7
KangwooChoi <mickeychoi83@gmail.com>
2025-03-11 10:49:26 +0900

    Time: 3 ms (42.3%), Space: 10.8 MB (47.74%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string> st;
-        char delimiter = ' ';
-        string word = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == delimiter) {
-                if (word.length() != 0) st.push(word);
-                word = "";
-                continue;
-            }
-            word += s[i];
-        } 
-        if (word.length() != 0) st.push(word);
         string ans = "";
-        while (!st.empty()) {
-            ans += st.top() + ' ';
-            st.pop(); 
+        stack<string> st;
+        istringstream is(s);
+        string word;
+        while (getline(is, word, ' ')) {
+            if (word.length() != 0) {
+                st.push(word);
+            }
         }
-        return ans.substr(0,ans.length()-1);
+        while (!st.empty()) { 
+            string curr = st.top();
+            st.pop();
+            if (curr.length() != 0) ans += curr + ' ';
+        }
+        return ans.substr(0, ans.length()-1);
     }
 };
+//class Solution {
+//public:
+//    string reverseWords(string s) {
+//        stack<string> st;
+//        char delimiter = ' ';
+//        string word = "";
+//        for (int i = 0; i < s.length(); i++) {
+//            if (s[i] == delimiter) {
+//                if (word.length() != 0) st.push(word);
+//                word = "";
+//                continue;
+//            }
+//            word += s[i];
+//        } 
+//        if (word.length() != 0) st.push(word);
+//        string ans = "";
+//        while (!st.empty()) {
+//            ans += st.top() + ' ';
+//            st.pop(); 
+//        }
+//        return ans.substr(0,ans.length()-1);
+//    }
+//};
