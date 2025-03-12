6d2489c6f0c29e452fba690e92ab09d95133706e
KangwooChoi <mickeychoi83@gmail.com>
2025-03-12 16:00:47 +0900

    Time: 39 ms (95.2%), Space: 18.8 MB (37.09%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for (int row = 0; row < matrix.size(); row++) {
-            if (matrix[row][0] > target) break;
-            for (int col = 0; col < matrix[0].size(); col++) {
-                if (matrix[row][col] == target) return true;
-                if (matrix[row][col] > target) break;
+        int row = matrix.size() - 1;
+        int col = 0;
+        while (0 <= row && col < matrix[0].size()) {
+            if (matrix[row][col] > target) {
+                row--;
+            } else if (matrix[row][col] < target) {
+                col++; 
+            } else {
+                return true;
             }
         }
         return false;
     }
 };
+//class Solution {
+//public:
+//    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//        for (int row = 0; row < matrix.size(); row++) {
+//            if (matrix[row][0] > target) break;
+//            for (int col = 0; col < matrix[0].size(); col++) {
+//                if (matrix[row][col] == target) return true;
+//                if (matrix[row][col] > target) break;
+//            }
+//        }
+//        return false;
+//    }
+//};
