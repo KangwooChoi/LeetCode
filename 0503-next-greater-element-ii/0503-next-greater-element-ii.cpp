d612fd82d119071aaa8a5a7d704cc8e6476c0b8b
KangwooChoi <mickeychoi83@gmail.com>
2025-03-12 12:26:15 +0900

    Time: 91 ms (9.17%), Space: 27.8 MB (98.87%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        // monotonic stack
+        // brute-force 
+        int n = nums.size();
+        vector<int> ans(n,-1);
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n+i+1; j++) {
+                if (nums[i] < nums[j%n]) {
+                    ans[i] = nums[j%n];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
