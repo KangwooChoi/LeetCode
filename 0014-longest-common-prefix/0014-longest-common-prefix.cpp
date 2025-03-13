3d0896445db33306263f04991bd92fb48f91ab18
KangwooChoi <mickeychoi83@gmail.com>
2025-03-13 13:00:59 +0900

    Time: 0 ms (100%), Space: 11.9 MB (69.99%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -7,6 +7,18 @@ public:
                 if (c != strs[j][i]) return strs[0].substr(0,i);
             }
         }
-        return strs[0]; 
+        return strs[0];
     }
 };
+//class Solution {
+//public:
+//    string longestCommonPrefix(vector<string>& strs) {
+//        for (int i = 0; i < strs[0].length(); i++) {
+//            char c = strs[0][i];
+//            for (int j = 1; j < strs.size(); j++) {
+//                if (c != strs[j][i]) return strs[0].substr(0,i);
+//            }
+//        }
+//        return strs[0]; 
+//    }
+//};
