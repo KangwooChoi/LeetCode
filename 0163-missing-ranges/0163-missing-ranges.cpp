14fff99d247d08a3bcef49a3ec9602b56c77cf46
KangwooChoi <mickeychoi83@gmail.com>
2025-03-13 13:06:25 +0900

    Time: 0 ms (100%), Space: 9.4 MB (0.97%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.cpp
+++ 0163-missing-ranges/0163-missing-ranges.cpp
@@ -3,17 +3,36 @@ public:
     vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper) {
         if (nums.size() == 0) return {{lower, upper}};
         vector<vector<int>> ans;
-        if (nums[0] != lower) {
+        if (lower != nums[0]) {
             ans.push_back({lower, nums[0]-1});
         }
         for (int i = 1; i < nums.size(); i++) {
             if (nums[i-1] + 1 != nums[i]) {
                 ans.push_back({nums[i-1]+1, nums[i]-1});
-            }
+            } 
         }
-        if (nums[nums.size()-1] != upper) {
-            ans.push_back({nums[nums.size()-1]+1, upper});
+        if (upper != nums[nums.size()-1]) {
+            ans.push_back({nums[nums.size()-1] + 1, upper});
         }
-        return ans; 
+        return ans;
     }
 };
+//class Solution {
+//public:
+//    vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper) {
+//        if (nums.size() == 0) return {{lower, upper}};
+//        vector<vector<int>> ans;
+//        if (nums[0] != lower) {
+//            ans.push_back({lower, nums[0]-1});
+//        }
+//        for (int i = 1; i < nums.size(); i++) {
+//            if (nums[i-1] + 1 != nums[i]) {
+//                ans.push_back({nums[i-1]+1, nums[i]-1});
+//            }
+//        }
+//        if (nums[nums.size()-1] != upper) {
+//            ans.push_back({nums[nums.size()-1]+1, upper});
+//        }
+//        return ans; 
+//    }
+//};
