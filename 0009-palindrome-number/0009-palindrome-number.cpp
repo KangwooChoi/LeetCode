2455b7cb14d0368e44a9c0500b84fd0371c74648
KangwooChoi <mickeychoi83@gmail.com>
2025-03-13 13:20:23 +0900

    Time: 3 ms (46.53%), Space: 8.6 MB (68.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,13 +2,27 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if (x == 0) return true;
-        if (x < 0 || x%10 == 0) return false;
+        if (x < 0 || x % 10 == 0) return false;
         int reverse = 0;
         while (x > reverse) {
             reverse *= 10;
             reverse += x % 10;
             x /= 10;
         }
-        return x == reverse || x == reverse / 10; 
+        return x == reverse || x == reverse / 10;
     }
 };
+//class Solution {
+//public:
+//    bool isPalindrome(int x) {
+//        if (x == 0) return true;
+//        if (x < 0 || x%10 == 0) return false;
+//        int reverse = 0;
+//        while (x > reverse) {
+//            reverse *= 10;
+//            reverse += x % 10;
+//            x /= 10;
+//        }
+//        return x == reverse || x == reverse / 10; 
+//    }
+//};
