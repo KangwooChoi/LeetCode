63a749bac830831ee57fedfdd3341a1a7ea52c7a
KangwooChoi <mickeychoi83@gmail.com>
2025-03-16 11:52:04 +0900

    Time: 4 ms (45.02%), Space: 55.1 MB (46.12%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,13 +4,27 @@ public:
         int n = nums.size();
         vector<bool> seen(n+1,false);
         for (int num : nums) {
-            if (num > 0 && num <= n) {
-                seen[num] = true;
-            }
+            if (num > 0 && num <= n) seen[num] = true;
         }
         for (int i = 1; i <= n; i++) {
-            if (seen[i] == false) return i;
+            if (!seen[i]) return i;
         }
-        return n+1; 
+        return n+1;
     }
 };
+//class Solution {
+//public:
+//    int firstMissingPositive(vector<int>& nums) {
+//        int n = nums.size();
+//        vector<bool> seen(n+1,false);
+//        for (int num : nums) {
+//            if (num > 0 && num <= n) {
+//                seen[num] = true;
+//            }
+//        }
+//        for (int i = 1; i <= n; i++) {
+//            if (seen[i] == false) return i;
+//        }
+//        return n+1; 
+//    }
+//};
