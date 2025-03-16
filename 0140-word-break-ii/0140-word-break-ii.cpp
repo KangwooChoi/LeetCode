a949ba3136a50332729167a243980c238ad3be0f
KangwooChoi <mickeychoi83@gmail.com>
2025-03-16 13:05:07 +0900

    Time: 2 ms (19.91%), Space: 10.4 MB (50.76%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<string> ans;
+
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         sort(wordDict.begin(), wordDict.end());
-        backtrack(s, wordDict, ""); 
+        backtrack(s, wordDict, "");
         return ans;
     }
+private:
     void backtrack(string s, vector<string>& wordDict, string res) {
         for (int i = 0; i < wordDict.size(); i++) {
             string word = wordDict[i];
@@ -15,10 +17,34 @@ public:
                     res += word;
                     ans.push_back(res);
                     return;
-                } else {
-                    backtrack(s.substr(word.length()), wordDict, res+word+" ");
                 }
+                backtrack(s.substr(word.length()), wordDict, res + word + " ");
             }
         }
+        return;
     }
 };
+//class Solution {
+//public:
+//    vector<string> ans;
+//    vector<string> wordBreak(string s, vector<string>& wordDict) {
+//        sort(wordDict.begin(), wordDict.end());
+//        backtrack(s, wordDict, ""); 
+//        return ans;
+//    }
+//    void backtrack(string s, vector<string>& wordDict, string res) {
+//        for (int i = 0; i < wordDict.size(); i++) {
+//            string word = wordDict[i];
+//            if (word.length() > s.length()) continue;
+//            if (word == s.substr(0,word.length())) {
+//                if (word.length() == s.length()) {
+//                    res += word;
+//                    ans.push_back(res);
+//                    return;
+//                } else {
+//                    backtrack(s.substr(word.length()), wordDict, res+word+" ");
+//                }
+//            }
+//        }
+//    }
+//};
