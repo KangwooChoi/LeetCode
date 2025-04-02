5bcf4d0e6f6e22fd3565e062b68ca6d52aeb1f48
KangwooChoi <mickeychoi83@gmail.com>
2025-04-02 10:22:43 +0900

    Time: 0 ms (100%), Space: 22.7 MB (12.05%) - LeetHub

--- 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.cpp
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        long long ans = 0;
         int n = nums.size();
-        for (int i = 0; i < n-2; i++) {
-            for (int j = i+1; j < n-1; j++) {
-                for (int k = j+1; k < n; k++) {
-                    if (nums[i] <= nums[j]) continue;
-                    long long temp = ((long)nums[i]-(long)nums[j]) * nums[k];
-                    ans = max(ans, temp);
-                }
-            }
+        vector<int> leftMax(n,0); 
+        vector<int> rightMax(n,0); 
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = max(leftMax[i-1], nums[i-1]);
+            rightMax[n-1-i] = max(rightMax[n-i], nums[n-i]);
         }
-        return ans; 
+        long long res = 0;
+        for (int i = 1; i < n-1; i++) {
+            res = max(res, (long long)(leftMax[i]-nums[i])*rightMax[i]);
+        }
+        return res;
     }
 };
+//class Solution {
+//public:
+//    long long maximumTripletValue(vector<int>& nums) {
+//        long long ans = 0;
+//        int n = nums.size();
+//        for (int i = 0; i < n-2; i++) {
+//            for (int j = i+1; j < n-1; j++) {
+//                for (int k = j+1; k < n; k++) {
+//                    if (nums[i] <= nums[j]) continue;
+//                    long long temp = ((long)nums[i]-(long)nums[j]) * nums[k];
+//                    ans = max(ans, temp);
+//                }
+//            }
+//        }
+//        return ans; 
+//    }
+//};
