d13191f62b6b1f29cf6a2b5223ab999e77422d8b
KangwooChoi <mickeychoi83@gmail.com>
2025-04-09 18:38:16 +0900

    Time: 0 ms (100%), Space: 24 MB (60.4%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -2,15 +2,15 @@ class NumArray {
 public:
     vector<int> partsum;
     NumArray(vector<int>& nums) {
-        partsum.push_back(nums[0]);
-        for (int i = 1; i < nums.size(); i++) {
+        partsum.push_back(0);
+        for (int i = 0; i < nums.size(); i++) {
             partsum.push_back(partsum.back()+nums[i]);
         } 
     }
     
     int sumRange(int left, int right) {
-        int sumRight = partsum[right]; 
-        int sumLeft = left == 0 ? 0 : partsum[left-1];
+        int sumRight = partsum[right+1]; 
+        int sumLeft = partsum[left]; 
         return sumRight-sumLeft;
     }
 };
