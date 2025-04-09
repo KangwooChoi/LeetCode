7f19ae113b4f18fd96043dda116d91f3317756f7
KangwooChoi <mickeychoi83@gmail.com>
2025-04-09 17:59:26 +0900

    Time: 0 ms (100%), Space: 8.4 MB (32.1%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.cpp
+++ 0266-palindrome-permutation/0266-palindrome-permutation.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool canPermutePalindrome(string s) {
-        map<char,int> freq;
+        vector<int> freq(26,0);
         for (char c : s) {
-            freq[c]++;
+            freq[c-'a']++;
         } 
-        bool existOdd = false; 
-        for (auto [c, f] : freq) {
-            if (f % 2 != 0) {
+        bool existOdd = false;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] % 2 != 0) {
                 if (existOdd) return false;
                 existOdd = true;
             }
-        }
+        } 
         return true;
     }
 };
