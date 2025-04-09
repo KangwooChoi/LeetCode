1bfbe0aaecbbfea19685635e0b83809c8c04d17b
KangwooChoi <mickeychoi83@gmail.com>
2025-04-09 17:19:55 +0900

    Time: 0 ms (100%), Space: 23.9 MB (17.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,10 +3,19 @@ public:
     void moveZeroes(vector<int>& nums) {
         int p = 0;
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) swap(nums[i], nums[p++]);    
+            if (nums[i] != 0) swap(nums[p++], nums[i]);
         }
     }
 };
+//class Solution {
+//public:
+//    void moveZeroes(vector<int>& nums) {
+//        int p = 0;
+//        for (int i = 0; i < nums.size(); i++) {
+//            if (nums[i] != 0) swap(nums[i], nums[p++]);    
+//        }
+//    }
+//};
 //class Solution {
 //public:
 //    void moveZeroes(vector<int>& nums) {
