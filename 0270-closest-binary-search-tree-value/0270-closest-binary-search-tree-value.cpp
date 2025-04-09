877d2a357db2b21ae1ff5498a42639ce555d93ca
KangwooChoi <mickeychoi83@gmail.com>
2025-04-09 17:43:07 +0900

    Time: 0 ms (100%), Space: 21.2 MB (49.67%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
@@ -11,27 +11,52 @@
  */
 class Solution {
 public:
-    int closest;
     int closestValue(TreeNode* root, double target) {
-        closest = INT_MAX;
-        helper(root, target);
-        return closest;
-    }
-private:
-    void helper(TreeNode* node, double target) {
-        if (node == nullptr) return;
-        if (abs(node->val-target) < abs(closest-target)) {
-            closest = node->val;
-        } else if (abs(node->val-target) == abs(closest-target) && node->val < closest) {
-            closest = node->val;
+        vector<int> vals;
+        while (root != nullptr) {
+            vals.push_back(root->val);
+            if (root->val == target) return root->val;
+            else if (root->val < target) root = root->right;
+            else if (root->val > target) root = root->left;
         }
-        if (node->val == target) {
-            return;
-        } else if (node->val > target) {
-            helper(node->left, target);
-        } else {
-            helper(node->right, target);
+        int ans = vals[0]; 
+        double diff = abs(target-vals[0]);
+        for (int i = 0; i < vals.size(); i++) {
+            double new_diff = abs(target-(double)vals[i]);
+            if (diff > new_diff) {
+                diff = new_diff;
+                ans = vals[i];
+            } else if (diff == new_diff && ans > vals[i]) {
+                diff = new_diff;
+                ans = vals[i];
+            }
         }
-
+        return ans;
     }
 };
+//class Solution {
+//public:
+//    int closest;
+//    int closestValue(TreeNode* root, double target) {
+//        closest = INT_MAX;
+//        helper(root, target);
+//        return closest;
+//    }
+//private:
+//    void helper(TreeNode* node, double target) {
+//        if (node == nullptr) return;
+//        if (abs(node->val-target) < abs(closest-target)) {
+//            closest = node->val;
+//        } else if (abs(node->val-target) == abs(closest-target) && node->val < closest) {
+//            closest = node->val;
+//        }
+//        if (node->val == target) {
+//            return;
+//        } else if (node->val > target) {
+//            helper(node->left, target);
+//        } else {
+//            helper(node->right, target);
+//        }
+//
+//    }
+//};
