e82f8e510de3927c18681dd520ab7cbccf7d8f4a
KangwooChoi <mickeychoi83@gmail.com>
2025-04-10 08:26:39 +0900

    Time: 6 ms (19.65%), Space: 11.8 MB (41.15%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,20 +1,54 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        vector<string> stack;
-        stringstream ss(path);
-        string temp;
-        while (getline(ss, temp, '/')) {
-            if (temp == "..") {
-                if (!stack.empty()) stack.pop_back();
-            } else if (temp != "." && !temp.empty()) {
-                stack.push_back(temp);
+        stack<string> st;
+        string word = "";
+        for (char c : path) {
+            if (c == '/') {
+                if (word != "") {
+                    if (word == "..") {
+                        if (!st.empty()) st.pop();
+                        word = "";
+                    } else if (word == ".") {
+                        word = "";
+                    } else {
+                        st.push(word);
+                        word = "";
+                    }
+                }
+            } else {
+                word += c;
             }
         }
-        string res = "";
-        for (auto str : stack) res += "/" + str;
-        if (res.empty()) return "/";
-        else return res;
+        if (!word.empty() && word != "." && word != "..") st.push(word);
+        if (word == ".." && !st.empty()) st.pop();
         
+        string ans = "";
+        while (!st.empty()) {
+            ans = '/' + st.top() + ans;
+            st.pop();
+        }
+        if (ans == "") return "/"; 
+        return ans;
     }
 };
+//class Solution {
+//public:
+//    string simplifyPath(string path) {
+//        vector<string> stack;
+//        stringstream ss(path);
+//        string temp;
+//        while (getline(ss, temp, '/')) {
+//            if (temp == "..") {
+//                if (!stack.empty()) stack.pop_back();
+//            } else if (temp != "." && !temp.empty()) {
+//                stack.push_back(temp);
+//            }
+//        }
+//        string res = "";
+//        for (auto str : stack) res += "/" + str;
+//        if (res.empty()) return "/";
+//        else return res;
+//        
+//    }
+//};
