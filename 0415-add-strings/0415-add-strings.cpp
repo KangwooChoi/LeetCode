61216f0368763b61f24f202e6152c1546397c743
KangwooChoi <mickeychoi83@gmail.com>
2025-04-09 22:31:34 +0900

    Time: 24 ms (14.89%), Space: 59.6 MB (12.67%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,54 +1,18 @@
-//class Solution {
-//public:
-//    string addStrings(string num1, string num2) {
-//        string ans = "";
-//        int carry = 0;
-//        int sum = 0;
-//        while (num1.length() != 0 && num2.length() != 0) {
-//            int n1 = num1.back() - '0';
-//            num1.pop_back();
-//            int n2 = num2.back() - '0';
-//            num2.pop_back();
-//            sum = n1 + n2 + carry;
-//            ans = to_string(sum%10) + ans; 
-//            carry = sum / 10;
-//        }
-//        while (num1.length() != 0) {
-//            int n1 = num1.back() - '0';
-//            num1.pop_back();
-//            sum = n1 + carry;
-//            ans = to_string(sum%10) + ans;
-//            carry = sum / 10;
-//        }
-//        while (num2.length() != 0) {
-//            int n2 = num2.back() - '0';
-//            num2.pop_back();
-//            sum = n2 + carry;
-//            ans = to_string(sum%10) + ans;
-//            carry = sum / 10;
-//        }
-//        if (carry) ans = to_string(carry) + ans;
-//        return ans;
-//    }
-//};
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        //return to_string(stoi(num1) + stoi(num2));
         string ans = "";
-        int p1 = num1.size() - 1, p2 = num2.size() - 1;
         int carry = 0;
-        while (p1 >= 0 || p2 >= 0 || carry) {
-            int digit1 = (p1 >= 0) ? num1[p1] - '0' : 0;
-            int digit2 = (p2 >= 0) ? num2[p2] - '0' : 0;
-            int total = digit1 + digit2 + carry;
-            carry = total / 10;
-
-            ans.push_back((total%10)+'0');
-            p1--;
-            p2--;
+        while(num1.length() != 0 || num2.length() != 0) {
+            int digit1 = num1.empty() ? 0 : num1.back() - '0';
+            int digit2 = num2.empty() ? 0 : num2.back() - '0';
+            if (!num1.empty()) num1.pop_back();
+            if (!num2.empty()) num2.pop_back();
+            int temp = digit1 + digit2 + carry;
+            ans = to_string(temp%10) + ans;
+            carry = temp / 10;
         }
-        reverse(ans.begin(), ans.end());
+        if (carry) ans = to_string(carry) + ans;
         return ans;
     }
 };
