f406de80c482c53f1c1e6a04a181c53be74dd7bb
KangwooChoi <mickeychoi83@gmail.com>
2025-04-10 10:35:39 +0900

    Time: 6 ms (92.26%), Space: 14.1 MB (96.11%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -8,30 +8,58 @@ public:
     Node* parent;
 };
 */
-
 class Solution {
 public:
-    Node* lowestCommonAncestor(Node* p, Node * q) {
-        stack<Node*> p_st;
-        stack<Node*> q_st;
-        helper(p, p_st); 
-        helper(q, q_st); 
-        Node* ans = nullptr;
-        while (!p_st.empty() && !q_st.empty()) {
-            if (p_st.top() == q_st.top()) {
-                ans = p_st.top();
-                p_st.pop();
-                q_st.pop();
-            } else {
-                break;
-            }
+    Node* lowestCommonAncestor(Node* p, Node* q) {
+        int p_depth = 0, q_depth = 0;
+        Node* tp = p;
+        Node* tq = q;
+        while (tp != nullptr) {
+            p_depth++;
+            tp = tp->parent;
         }
-        return ans;
-    }
-private:
-    void helper(Node* p, stack<Node*>& st) {
-        if (p == nullptr) return;
-        st.push(p);
-        helper(p->parent, st);
-    }
+        while (tq != nullptr) {
+            q_depth++;
+            tq = tq->parent;
+        }
+        while (p_depth > q_depth) {
+            p = p->parent;
+            p_depth--;
+        }
+        while (p_depth < q_depth) {
+            q = q->parent;
+            q_depth--;
+        }
+        while (p != q) {
+            p = p->parent;
+            q = q->parent;
+        }
+        return p;
+    }   
 };
+//class Solution {
+//public:
+//    Node* lowestCommonAncestor(Node* p, Node * q) {
+//        stack<Node*> p_st;
+//        stack<Node*> q_st;
+//        helper(p, p_st); 
+//        helper(q, q_st); 
+//        Node* ans = nullptr;
+//        while (!p_st.empty() && !q_st.empty()) {
+//            if (p_st.top() == q_st.top()) {
+//                ans = p_st.top();
+//                p_st.pop();
+//                q_st.pop();
+//            } else {
+//                break;
+//            }
+//        }
+//        return ans;
+//    }
+//private:
+//    void helper(Node* p, stack<Node*>& st) {
+//        if (p == nullptr) return;
+//        st.push(p);
+//        helper(p->parent, st);
+//    }
+//};
