774b1864760d917b5d00d214d6f908d617e688b3
KangwooChoi <mickeychoi83@gmail.com>
2025-04-10 11:44:39 +0900

    Time: 25 ms (90.25%), Space: 68.3 MB (72.5%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -3,16 +3,35 @@ public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         priority_queue<pair<int,int>> pq;
         for (int i = 0; i < points.size(); i++) {
-            int dist = points[i][0]*points[i][0] + points[i][1]*points[i][1]; 
-            pq.push({dist,i});
-            if (pq.size() > k) pq.pop();
+            int x = points[i][0], y = points[i][1];
+            int dist = x*x + y*y; 
+            pq.push({-dist,i});
         }
         vector<vector<int>> ans;
-        while (!pq.empty()) {
+        while (k > 0) {
             auto [dist, index] = pq.top();
             ans.push_back(points[index]);
             pq.pop();
+            k--;
         }
         return ans;
     }
 };
+//class Solution {
+//public:
+//    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+//        priority_queue<pair<int,int>> pq;
+//        for (int i = 0; i < points.size(); i++) {
+//            int dist = points[i][0]*points[i][0] + points[i][1]*points[i][1]; 
+//            pq.push({dist,i});
+//            if (pq.size() > k) pq.pop();
+//        }
+//        vector<vector<int>> ans;
+//        while (!pq.empty()) {
+//            auto [dist, index] = pq.top();
+//            ans.push_back(points[index]);
+//            pq.pop();
+//        }
+//        return ans;
+//    }
+//};
