96e38eedb3941334a5e25e8f47b0f2edfac1ef8a
KangwooChoi <mickeychoi83@gmail.com>
2025-04-10 11:38:08 +0900

    Time: 85 ms (19.09%), Space: 146.2 MB (69.44%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        int prefixMod = 0;
-        unordered_map<int,int> modSeen;
-        modSeen[0] = -1;
+        int prefix = 0;
+        unordered_map<int,int> seen;
+        seen[0] = -1;    
         for (int i = 0; i < nums.size(); i++) {
-            prefixMod = (prefixMod + nums[i]) % k;
-            if (modSeen.find(prefixMod) != modSeen.end()) {
-                if (i - modSeen[prefixMod] > 1) {
-                    return true;
-                }
+            prefix = (prefix + nums[i]) % k;
+            if (seen.find(prefix) != seen.end()) {
+                if (i - seen[prefix] > 1) return true;
             } else {
-                modSeen[prefixMod] = i;
+                seen[prefix] = i;
             }
         }
         return false;
     }
 };
-//class Solution {
-//public:
-//    bool checkSubarraySum(vector<int>& nums, int k) {
-//        for (int i = 0; i < nums.size(); i++) {
-//            int partSum = nums[i];
-//            for (int j = i+1; j < nums.size(); j++) {
-//                partSum += nums[j];
-//                if (partSum % k == 0) return true;
-//            }
-//        }
-//        return false; 
-//    }
-//};
