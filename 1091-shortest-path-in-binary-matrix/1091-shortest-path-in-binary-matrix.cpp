4cfeef4d0875f72a27c518bc958ff400b4eca0a3
KangwooChoi <mickeychoi83@gmail.com>
2025-04-10 13:57:44 +0900

    Time: 324 ms (5.02%), Space: 101.3 MB (5.02%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1}};
-
+    vector<vector<int>> dirs = {{1,-1}, {1,0}, {1,1}, {0,-1}, {0,1}, {-1,-1}, {-1,0}, {-1,1}};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        if (grid[0][0] != 0 || grid[m-1][n-1] != 0) return -1;
-        queue<vector<int>> q;
+        int n = grid.size();
+        if (grid[0][0] != 0 || grid[n-1][n-1] != 0) return -1; 
+        queue<vector<int>> q; 
+        grid[0][0] = 2;
         q.push({0,0,1});
         while (!q.empty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                auto curr = q.front();
-                q.pop();
-                if (curr[0] == m-1 && curr[1] == n-1) return curr[2];
-                for (int k = 0; k < 8; k++) {
-                    int nx = curr[0] + dir[k][0];
-                    int ny = curr[1] + dir[k][1];
-                    if (0 <= nx && nx < m && 0 <= ny && ny < n && grid[nx][ny] == 0) {
-                        grid[nx][ny] = 2;
-                        q.push({nx,ny,curr[2]+1});
+            vector<int> curr = q.front();
+            q.pop();
+            int row = curr[0], col = curr[1], step = curr[2];
+            if (row == n-1 && col == n-1) return step;
+            for (int k = 0; k < 8; k++) {
+                auto dir = dirs[k];
+                int nrow = row + dir[0];
+                int ncol = col + dir[1];
+                if (0 <= nrow && nrow < n && 0 <= ncol && ncol < n) {
+                    if (nrow == n-1 && ncol == n-1) return step+1;
+                    if (grid[nrow][ncol] == 0) {
+                        grid[nrow][ncol] = 2;
+                        q.push({nrow,ncol,step+1});
                     }
                 }
             }
