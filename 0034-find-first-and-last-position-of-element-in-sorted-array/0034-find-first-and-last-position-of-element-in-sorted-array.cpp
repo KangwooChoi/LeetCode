1e120209610c2eb070e95d66040afa349142cbdb
KangwooChoi <mickeychoi83@gmail.com>
2025-04-10 11:30:36 +0900

    Time: 0 ms (100%), Space: 17.6 MB (17.88%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,32 +1,63 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int firstOccurence = findBound(nums, target, true);
-        if (firstOccurence == -1) return {-1,-1};
-        int lastOccurence = findBound(nums, target, false);  
-        return {firstOccurence, lastOccurence};
+        int left = -1, right = -1;
+        left = binarysearch(nums, target, true);
+        right = binarysearch(nums, target, false);
+        return {left, right};
     }
+
 private:
-    int findBound(vector<int>& nums, int target, bool isFirst) {
-        int n = nums.size(); 
-        int left = 0, right = n-1;
+    int binarysearch(vector<int>& nums, int target, bool isLeft) {
+        int left = 0, right = nums.size() - 1;
         while (left <= right) {
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                if (isFirst) {
+                if (isLeft) {
                     if (left == mid || nums[mid-1] != target) return mid;
                     right = mid - 1;
                 } else {
                     if (right == mid || nums[mid+1] != target) return mid;
                     left = mid + 1;
                 }
-
             } else if (nums[mid] > target) {
                 right = mid - 1;
-            } else if (nums[mid] < target) {
+            } else {
                 left = mid + 1;
             }
         }
         return -1;
     }
 };
+//class Solution {
+//public:
+//    vector<int> searchRange(vector<int>& nums, int target) {
+//        int firstOccurence = findBound(nums, target, true);
+//        if (firstOccurence == -1) return {-1,-1};
+//        int lastOccurence = findBound(nums, target, false);  
+//        return {firstOccurence, lastOccurence};
+//    }
+//private:
+//    int findBound(vector<int>& nums, int target, bool isFirst) {
+//        int n = nums.size(); 
+//        int left = 0, right = n-1;
+//        while (left <= right) {
+//            int mid = (left + right) / 2;
+//            if (nums[mid] == target) {
+//                if (isFirst) {
+//                    if (left == mid || nums[mid-1] != target) return mid;
+//                    right = mid - 1;
+//                } else {
+//                    if (right == mid || nums[mid+1] != target) return mid;
+//                    left = mid + 1;
+//                }
+//
+//            } else if (nums[mid] > target) {
+//                right = mid - 1;
+//            } else if (nums[mid] < target) {
+//                left = mid + 1;
+//            }
+//        }
+//        return -1;
+//    }
+//};
