b0044b18600b76141082c80500909f54403446d5
KangwooChoi <mickeychoi83@gmail.com>
2025-07-08 10:01:27 +0900

    Time: 28 ms (16.36%), Space: 24.1 MB (6.32%) - LeetHub

---
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        vector<TreeNode*> arr;
+        helper(root, arr);
+        int key = p->val;
+        int ans = -1;
+        for (int i = 0; i < arr.size() - 1; i++) {
+            if (arr[i]->val == key) {
+                ans = i + 1;
+                break;
+            }
+        }
+        return ans != -1 ? arr[ans] : nullptr;
+    }
+    
+    void helper(TreeNode* node, vector<TreeNode*>& arr) {
+        if (node == nullptr) return;
+        if (node->left) helper(node->left, arr);
+        arr.push_back(node);
+        if (node->right) helper(node->right, arr);
+        return;
+    }
+};
