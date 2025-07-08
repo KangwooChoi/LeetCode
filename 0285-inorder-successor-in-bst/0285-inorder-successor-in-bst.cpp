f8e6d6ef2858853ed132f43753ef1d9db6510cc9
KangwooChoi <mickeychoi83@gmail.com>
2025-07-08 10:05:45 +0900

    Time: 32 ms (7.99%), Space: 23.1 MB (15.06%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.cpp
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.cpp
@@ -10,24 +10,39 @@
 class Solution {
 public:
     TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-        vector<TreeNode*> arr;
-        helper(root, arr);
-        int key = p->val;
-        int ans = -1;
-        for (int i = 0; i < arr.size() - 1; i++) {
-            if (arr[i]->val == key) {
-                ans = i + 1;
-                break;
+        TreeNode* ans = nullptr;
+        while (root != nullptr) {
+            if (p->val >= root->val) {
+                root = root->right;
+            } else {
+                ans = root; 
+                root = root->left;
             }
         }
-        return ans != -1 ? arr[ans] : nullptr;
-    }
-    
-    void helper(TreeNode* node, vector<TreeNode*>& arr) {
-        if (node == nullptr) return;
-        if (node->left) helper(node->left, arr);
-        arr.push_back(node);
-        if (node->right) helper(node->right, arr);
-        return;
+        return ans;
     }
 };
+//class Solution {
+//public:
+//    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+//        vector<TreeNode*> arr;
+//        helper(root, arr);
+//        int key = p->val;
+//        int ans = -1;
+//        for (int i = 0; i < arr.size() - 1; i++) {
+//            if (arr[i]->val == key) {
+//                ans = i + 1;
+//                break;
+//            }
+//        }
+//        return ans != -1 ? arr[ans] : nullptr;
+//    }
+//    
+//    void helper(TreeNode* node, vector<TreeNode*>& arr) {
+//        if (node == nullptr) return;
+//        if (node->left) helper(node->left, arr);
+//        arr.push_back(node);
+//        if (node->right) helper(node->right, arr);
+//        return;
+//    }
+//};
