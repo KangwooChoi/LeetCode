cf110db10c52bb689201f582b5c581d9238ffacb
KangwooChoi <mickeychoi83@gmail.com>
2025-07-15 09:38:23 +0900

    Time: 0 ms (100%), Space: 7.9 MB (67.48%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        if (word.length() < 3) return false; 
+        bool hasVowel = false, hasConsonant = false;
+        for (char c : word) {
+            if (!isalnum(c)) return false;
+            if (isdigit(c)) continue;
+            c = tolower(c);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ) hasVowel = true; 
+            else hasConsonant = true;
+        }
+        return hasVowel && hasConsonant;
+    }
+};
