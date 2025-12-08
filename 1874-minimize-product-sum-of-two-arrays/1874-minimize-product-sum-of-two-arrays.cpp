5d3837f0c82c1f9b9c5fac1fe6c3b559339c4f2e
KangwooChoi <mickeychoi83@gmail.com>
2025-12-08 13:38:30 +0900

    Time: 117 ms (93.55%), Space: 109.3 MB (58.06%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.cpp
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.cpp
@@ -3,7 +3,11 @@ public:
     int minProductSum(vector<int>& nums1, vector<int>& nums2) {
         sort(nums1.begin(), nums1.end());
         //sort(nums2.rbegin(), nums2.rend());
-        sort(nums2.begin(), nums2.end(), greater<int>());
+        //sort(nums2.begin(), nums2.end(), greater<int>());
+        sort(nums2.begin(), nums2.end(),
+                    [](int a, int b) {
+                        return a > b;
+                    });
         int ans = 0;
         int n = nums1.size();
         for (int i = 0; i < n; i++) {
